48b23721759f27914d445f420b1af0b524fd036e
johanneshilden <hildenjohannes@gmail.com>
2017-06-28 12:52:11 +0300

    Update build

--- koa2/dist/src/api.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Router = require("koa-router");
-var Api;
-(function (Api) {
-    Api.router = new Router();
-})(Api = exports.Api || (exports.Api = {}));
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBpLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vc3JjL2FwaS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLHFDQUFxQztBQUVyQyxJQUFjLEdBQUcsQ0FJaEI7QUFKRCxXQUFjLEdBQUc7SUFFSixVQUFNLEdBQVcsSUFBSSxNQUFNLEVBQUUsQ0FBQztBQUUzQyxDQUFDLEVBSmEsR0FBRyxHQUFILFdBQUcsS0FBSCxXQUFHLFFBSWhCIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0ICogYXMgUm91dGVyIGZyb20gJ2tvYS1yb3V0ZXInO1xuXG5leHBvcnQgbW9kdWxlIEFwaSB7XG5cbiAgZXhwb3J0IGxldCByb3V0ZXI6IFJvdXRlciA9IG5ldyBSb3V0ZXIoKTtcblxufVxuIl19
