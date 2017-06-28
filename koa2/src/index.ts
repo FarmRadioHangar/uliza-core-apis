b690bfbd543bb4ce35bf8e2d7e3269cb54b701f3
johanneshilden <hildenjohannes@gmail.com>
2017-06-28 12:27:54 +0300

    Add cors middleware

--- koa2/src/index.ts
+++ koa2/src/index.ts
@@ -1,10 +1,12 @@
 import * as Koa        from 'koa';
 import * as bodyparser from 'koa-bodyparser';
 import * as logger     from 'koa-logger';
+import * as cors       from 'kcors';
 
 let app: Koa = new Koa();
 
-app.use(logger())
+app.use(cors())
+   .use(logger())
    .use(bodyparser());
 
 app.listen(3000);
