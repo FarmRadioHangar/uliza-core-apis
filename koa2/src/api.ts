dd785900c6fe4a4f66cf473d21d1e5fbbe2465c0
johanneshilden <hildenjohannes@gmail.com>
2017-06-28 13:18:47 +0300

    Create base route request handler

--- koa2/src/api.ts
+++ koa2/src/api.ts
@@ -2,6 +2,14 @@ import * as Router from 'koa-router';
 
 export module Api {
 
-  export let router: Router = new Router();
+  let api: Router = new Router();
+
+  api.get('/', async (ctx, next) => {
+    await next();
+    ctx.body = 'body';
+    ctx.status = 200;
+  });
+
+  export const router = api;
 
 }
