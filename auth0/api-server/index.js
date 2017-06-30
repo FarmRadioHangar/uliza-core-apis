456003f2310535324d3013d007239cdad62e72c7
johanneshilden <hildenjohannes@gmail.com>
2017-06-30 10:55:49 +0300

    Add secure API server example

---
+++ auth0/api-server/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
+
+var app = express();
+var port = process.env.PORT || 8080;
+
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: "https://farmradio.eu.auth0.com/.well-known/jwks.json"
+  }),
+  audience: 'https://dev.farmradio.fm/api/',
+  issuer: "https://farmradio.eu.auth0.com/",
+  algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
+
+app.get('/authorized', function(req, res) {
+  res.send('Secured Resource');
+});
+
+console.log('Listening on port ' + port);
+
+app.listen(port);
