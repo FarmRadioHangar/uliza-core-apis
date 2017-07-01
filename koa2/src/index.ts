f614c659e1376fd74cb231c0591c3d684ede35f9
Johannes Hilden <johannes@isomorphic.co>
2017-07-01 15:39:41 +0300

    Add dom to lib array

--- koa2/src/index.ts
+++ koa2/src/index.ts
@@ -1,25 +1,21 @@
 import 'reflect-metadata';
 
-import * as Koa from 'koa';
-import * as Router from 'koa-router';
-import * as bodyparser from 'koa-bodyparser';
+import * as Koa          from 'koa';
+import * as Router       from 'koa-router';
+import * as bodyparser   from 'koa-bodyparser';
 
-import { createConnection } from 'typeorm';
-
-import { getEntityManager } from 'typeorm';
 import { Organizations } from './entity/Organizations';
 
-createConnection().then(async connection => {
+import { Connection, createConnection } from 'typeorm';
+
+createConnection().then(connection => {
 
   let app: Koa = new Koa();
   let router: Router = new Router();
 
-  router.get('/organizations', async (ctx, next) => {
-    await next();
-
-    const repository = getEntityManager().getRepository(Organizations);
+  router.get('/organizations', async ctx => {
+    const repository = connection.getRepository(Organizations);
     const organizations = await repository.find();
-
     ctx.body = { collection: organizations };
   });
 
@@ -33,5 +29,5 @@ createConnection().then(async connection => {
   console.log(`Koa application is up and running on port ${port}`);
 
 }).catch(error => {
-  console.log(console.log('Connection error'));
+  console.log('Connection error');
 });
