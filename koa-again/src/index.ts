fe87719116ba7053cc32dd657f3be4d08d3d010a
Johannes Hilden <johannes@isomorphic.co>
2017-07-01 21:55:11 +0300

    Implement jwt middleware

--- koa-again/src/index.ts
+++ koa-again/src/index.ts
@@ -1,19 +1,43 @@
 import * as Koa        from 'koa';
 import * as Router     from 'koa-router';
 import * as bodyparser from 'koa-bodyparser';
+import * as jwt        from 'koa-jwt';
 
 let app    = new Koa(),
     router = new Router();
 
-router.get('/organizations', async (ctx: Koa.Context) => {
+router.get('/organizations', async ctx => {
   ctx.body = { collection: [] };
 });
 
-app.use(bodyparser())
+router.get('/protected', async ctx => {
+  ctx.body = { message: 'This API is a teapot.' };
+});
+
+interface JwtOptions extends jwt.Options {
+  audience?: string,
+  issuer?: string,
+  algorithms?: Array<string>
+}
+
+const options: JwtOptions = { 
+  secret: require('jwks-rsa').koaJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://farmradio.eu.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'https://dev.farmradio.fm/api/',
+  issuer: 'https://farmradio.eu.auth0.com/',
+  algorithms: ['RS256']
+};
+
+app.use(jwt(options))
+   .use(bodyparser())
    .use(router.routes())
    .use(router.allowedMethods());
 
-app.listen(3030);
+app.listen(8080);
 
 console.log('Koa application is up and running');
 
