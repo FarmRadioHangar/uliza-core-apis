fbc6dcef37ef7688d54b4e77994f91c4219fe2a7
Johannes Hilden <johannes@isomorphic.co>
2017-07-02 00:31:53 +0300

    Get some data from db using knex

--- koa-again/src/index.ts
+++ koa-again/src/index.ts
@@ -2,47 +2,57 @@ import * as Koa        from 'koa';
 import * as Router     from 'koa-router';
 import * as bodyparser from 'koa-bodyparser';
 import * as jwt        from 'koa-jwt';
+import * as Knex       from 'knex';
 
 const env  = process.env.NODE_ENV || 'development';
 
 let app    = new Koa(),
     router = new Router();
 
+const knex = Knex({
+  client: 'sqlite3',
+  connection: { 
+    filename: './db.sqlite' 
+  }
+});
+
 router.get('/organizations', async ctx => {
-  ctx.body = { collection: [] };
+  const collection = await knex('organizations');
+  ctx.body = { collection };
 });
 
 router.get('/protected', async ctx => {
   ctx.body = { message: 'This API is a teapot.' };
 });
 
-interface JwtOptions extends jwt.Options {
-  audience?: string,
-  issuer?: string,
-  algorithms?: Array<string>
-}
-
-const options: JwtOptions = { 
-  secret: require('jwks-rsa').koaJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: 'https://farmradio.eu.auth0.com/.well-known/jwks.json'
-  }),
-  audience: 'https://dev.farmradio.fm/api/',
-  issuer: 'https://farmradio.eu.auth0.com/',
-  algorithms: ['RS256']
-};
-
 if ('test' !== env) {
+
+  interface JwtOptions extends jwt.Options {
+    audience?: string,
+    issuer?: string,
+    algorithms?: Array<string>
+  }
+
+  const options: JwtOptions = { 
+    secret: require('jwks-rsa').koaJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://farmradio.eu.auth0.com/.well-known/jwks.json'
+    }),
+    audience: 'https://dev.farmradio.fm/api/',
+    issuer: 'https://farmradio.eu.auth0.com/',
+    algorithms: ['RS256']
+  };
+
   app.use(jwt(options));
+
 }
 
 app.use(bodyparser())
    .use(router.routes())
-   .use(router.allowedMethods());
-
-app.listen(8080);
+   .use(router.allowedMethods())
+   .listen(8080);
 
 console.log('Koa application is up and running');
 
