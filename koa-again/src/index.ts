fe72b57a7535265e2cfa6a6c3d9f53b97a08d2b9
Johannes Hilden <johannes@isomorphic.co>
2017-07-02 02:31:56 +0300

    Read db configuration from knexfile

--- koa-again/src/index.ts
+++ koa-again/src/index.ts
@@ -2,18 +2,15 @@ import * as Koa        from 'koa';
 import * as Router     from 'koa-router';
 import * as bodyparser from 'koa-bodyparser';
 import * as knex       from 'knex';
+import * as path       from 'path';
 
 import { Auth0 } from './auth0';
 
-const env  = process.env.NODE_ENV || 'development';
+const env  = process.env.NODE_ENV || 'development',
+      db   = knex(require(path.join(__dirname, '../knexfile.js'))[env]);
 
 let app    = new Koa(),
     router = new Router();
-
-const db = knex({
-  client: 'sqlite3',
-  connection: { filename: './db.sqlite' }
-});
 
 router.get('/organizations', async ctx => {
   const collection = await db('organizations');
