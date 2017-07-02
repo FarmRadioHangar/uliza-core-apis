3552c16d9640ae1b38b23d3271c0824acd9451c6
Johannes Hilden <johannes@isomorphic.co>
2017-07-02 19:24:32 +0300

    Run migration before test

--- server/src/tests/test.ts
+++ server/src/tests/test.ts
@@ -1,17 +1,28 @@
-import { expect }     from 'chai';
-import * as agent     from 'supertest-koa-agent';
-import app            from '../index';
+import { expect } from 'chai';
+import * as agent from 'supertest-koa-agent';
+import * as knex  from 'knex';
+import * as path  from 'path';
+import app        from '../index';
+
+const db = knex(require(path.join(__dirname, '../../knexfile.js')).test);
 
 describe('GET /organizations', () => {
+
+  before(() => {
+    db.migrate.latest();
+  });
+
   it('should return JSON', (done) => {
     agent(app)
       .get('/organizations')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/, done);
   });
+
   it('should return 200 OK', (done) => {
     agent(app)
       .get('/organizations')
       .expect(200, done);
   });
+
 });
