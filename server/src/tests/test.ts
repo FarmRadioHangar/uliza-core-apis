396e14e50ee31b76f9a1e15aa790bb4530409040
Johannes Hilden <johannes@isomorphic.co>
2017-07-02 22:09:07 +0300

    Add a should-style test

--- server/src/tests/test.ts
+++ server/src/tests/test.ts
@@ -1,9 +1,11 @@
-import { expect } from 'chai';
+import * as chai  from 'chai';
 import * as agent from 'supertest-koa-agent';
 import { Db }     from '../db';
 import app        from '../index';
 
-const db = Db.connection();
+const should = chai.should(),
+      expect = chai.expect,
+      db = Db.connection();
 
 describe('GET /organizations', () => {
 
@@ -19,10 +21,16 @@ describe('GET /organizations', () => {
       .expect('Content-Type', /json/);
   });
 
-  it('should return 200 OK', async () => {
-    agent(app)
+  it('should respond with 200 OK', async () => {
+    await agent(app)
       .get('/organizations')
       .expect(200);
+  });
+
+  it('should return a collection of 3 items', async () => {
+    const response = await agent(app).get('/organizations');
+    response.body.should.have.property('collection');
+    response.body.collection.length.should.equal(3);
   });
 
 });
