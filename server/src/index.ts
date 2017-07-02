be7d993d921d8552730bbcf9a5942b38da71733c
Johannes Hilden <johannes@isomorphic.co>
2017-07-02 17:56:53 +0300

    Align code

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,8 @@ import * as Koa        from 'koa';
 import * as bodyparser from 'koa-bodyparser';
 
 import { Auth0 } from './auth0';
-import router from './router';
-import db from './db';
+import router    from './router';
+import db        from './db';
 
 const env = process.env.NODE_ENV || 'development',
       app = new Koa();
