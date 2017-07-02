34cf41e1f1e5e04ac894e3cd7b74475e9ed360aa
Johannes Hilden <johannes@isomorphic.co>
2017-07-02 21:22:43 +0300

    Remove misplaced semicolon

--- server/migrations/20170702004807_create_organizations.js
+++ server/migrations/20170702004807_create_organizations.js
@@ -5,7 +5,6 @@ exports.up = function(knex, Promise) {
     knex.schema.createTable('organizations', function(table) {
       table.increments();
       table.string('name').notNullable();
-      //table.timestamps();
       table.timestamp('created_at').defaultTo(knex.fn.now());
       table.timestamp('updated_at').defaultTo(knex.fn.now());
     })
@@ -14,6 +13,6 @@ exports.up = function(knex, Promise) {
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.dropTable('organizations');
+    knex.schema.dropTable('organizations')
   ]); 
 };
