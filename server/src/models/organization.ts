b88545319db32ec5c05f57fda4717376537a5ea2
Johannes Hilden <johannes@isomorphic.co>
2017-07-04 03:05:31 +0300

    Make tableName a static property

--- server/src/models/organization.ts
+++ server/src/models/organization.ts
@@ -2,8 +2,6 @@ import { Model } from 'objection';
 
 export default class Organization extends Model {
 
-  static get tableName() {
-    return 'organizations';
-  }
+  static tableName: string = 'organizations';
 
 }
