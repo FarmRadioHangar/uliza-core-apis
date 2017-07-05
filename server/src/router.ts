bfd4935ea36252f80bdffba8d16bd69d9c71aef4
johanneshilden <hildenjohannes@gmail.com>
2017-07-05 15:22:20 +0300

    Add support for offset and limit params

--- server/src/router.ts
+++ server/src/router.ts
@@ -6,13 +6,25 @@ import Organization from './models/organization';
 export default (api: Router) => {
 
   api.get('/organizations', async ctx => {
-    const collection = await find(Organization).build(ctx.query);
+    const { offset, limit, ...params } = ctx.query;
+    const collection = await find(Organization)
+      .build(params)
+      .skipUndefined()
+      .offset(offset)
+      .limit(limit);
     ctx.body = { collection };
   });
 
+  api.get('/organizations/:id', async ctx => {
+    const results = await Organization.query().where('id', '=', ctx.params.id);
+    if (results.length === 1) {
+      ctx.body = results[0];
+    } 
+  });
+
   api.get('/organizations/count', async ctx => {
-    const result = await Organization.query().count();
-    ctx.body = { count: result[0]['count(*)'] };
+    const results = await Organization.query().count();
+    ctx.body = { count: results[0]['count(*)'] };
   });
 
   api.get('/protected', async ctx => {
