5820a6e21ff462a83ace11f7bf5b02c9727159a7
johanneshilden <hildenjohannes@gmail.com>
2017-07-06 17:16:10 +0300

    Create npm script to load seeds

--- server/migrations/20170702004807_create_organizations.js
+++ server/migrations/20170702004807_create_organizations.js
@@ -1,18 +1,14 @@
 'use strict';
 
 exports.up = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.createTable('organizations', function(table) {
-      table.increments();
-      table.string('name').notNullable();
-      table.timestamp('created_at').defaultTo(knex.fn.now());
-      table.timestamp('updated_at').defaultTo(knex.fn.now());
-    })
-  ]);
+  return knex.schema.createTable('organizations', function(table) {
+    table.increments();
+    table.string('name').notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  })
 };
 
 exports.down = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.dropTable('organizations')
-  ]); 
+  return knex.schema.dropTable('organizations')
 };
--- server/seeds/dev/organizations.js
+++ server/seeds/dev/organizations.js
@@ -4,30 +4,28 @@ exports.seed = function(knex, Promise) {
   return knex('organizations')
     .del()
     .then(function() {
-      return Promise.all([
-        knex('organizations').insert([
-          { id: 1,  name: 'Universal Exports' },
-          { id: 2,  name: 'Mystery Incorporated' },
-          { id: 3,  name: 'Lunar Industries Ltd.' },
-          { id: 4,  name: 'Tyrell Corporation' },
-          { id: 5,  name: 'Zorin Industries' },
-          { id: 6,  name: 'U.N.C.L.E.' },
-          { id: 7,  name: 'Reynholm Industries' },
-          { id: 8,  name: 'IZON Research Facilities' },
-          { id: 9,  name: 'Bluebook' },
-          { id: 10, name: 'Omni Presents' },
-          { id: 11, name: 'Bendini, Lambert & Locke' },
-          { id: 12, name: 'Omni Consumer Products' },
-          { id: 13, name: 'Destroido Corp.' },
-          { id: 14, name: 'Icebergs International' },
-          { id: 15, name: 'Vandelay Industries' },
-          { id: 16, name: 'Stark Industries' },
-          { id: 17, name: 'Burns Industries' },
-          { id: 18, name: 'Cyberdyne Systems' },
-          { id: 19, name: 'Los Pollos Hermanos' },
-          { id: 20, name: 'Umbrella Corporation' },
-          { id: 21, name: 'Planet Express, Inc.' }
-        ])
-      ]);
+      return knex('organizations').insert([
+        { id: 1,  name: 'Universal Exports' },
+        { id: 2,  name: 'Mystery Incorporated' },
+        { id: 3,  name: 'Lunar Industries Ltd.' },
+        { id: 4,  name: 'Tyrell Corporation' },
+        { id: 5,  name: 'Zorin Industries' },
+        { id: 6,  name: 'U.N.C.L.E.' },
+        { id: 7,  name: 'Reynholm Industries' },
+        { id: 8,  name: 'IZON Research Facilities' },
+        { id: 9,  name: 'Bluebook' },
+        { id: 10, name: 'Omni Presents' },
+        { id: 11, name: 'Bendini, Lambert & Locke' },
+        { id: 12, name: 'Omni Consumer Products' },
+        { id: 13, name: 'Destroido Corp.' },
+        { id: 14, name: 'Icebergs International' },
+        { id: 15, name: 'Vandelay Industries' },
+        { id: 16, name: 'Stark Industries' },
+        { id: 17, name: 'Burns Industries' },
+        { id: 18, name: 'Cyberdyne Systems' },
+        { id: 19, name: 'Los Pollos Hermanos' },
+        { id: 20, name: 'Umbrella Corporation' },
+        { id: 21, name: 'Planet Express, Inc.' }
+      ])
     });
 };
