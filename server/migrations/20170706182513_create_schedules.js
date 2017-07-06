e2479853727704a81fe7e59a7be1af0d8e04ac2a
Johannes Hilden <johannes@isomorphic.co>
2017-07-06 18:32:15 +0300

    Scaffold out some table migrations

--- server/migrations/20170706182513_create_schedules.js
+++ server/migrations/20170706182513_create_schedules.js
@@ -1,14 +1,13 @@
 'use strict';
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('organizations', function(table) {
+  return knex.schema.createTable('schedules', function(table) {
     table.increments();
-    table.string('name').notNullable();
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
   });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('organizations');
+  return knex.schema.dropTable('schedules');
 };
