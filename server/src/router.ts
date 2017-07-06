c21886100b38b3a7fe02e4beb191a2117b1a8ac3
Johannes Hilden <johannes@isomorphic.co>
2017-07-06 09:29:08 +0300

    Clean up router code

--- server/src/router.ts
+++ server/src/router.ts
@@ -3,28 +3,57 @@ import * as find   from 'objection-find';
 
 import Organization from './models/organization';
 
+function fields(select: string|undefined): Array<string>|undefined {
+  if ('string' !== typeof select) {
+    return undefined;
+  }
+  return select.split(',');
+}
+
 export default (api: Router) => {
 
+  /**
+   * GET /organizations
+   */
   api.get('/organizations', async ctx => {
-    const { offset, limit, ...params } = ctx.query;
+    const { select, offset, limit, ...params } = ctx.query;
     const collection = await find(Organization)
       .build(params)
       .skipUndefined()
+      .select(fields(select))
       .offset(offset)
       .limit(limit);
     ctx.body = { collection };
   });
 
-  api.get('/organizations/:id', async ctx => {
-    const results = await Organization.query().where('id', '=', ctx.params.id);
-    if (results.length === 1) {
-      ctx.body = results[0];
-    } 
+  /**
+   * GET /organizations/count
+   */
+  api.get('/organizations/count', async ctx => {
+    const { select, offset, limit, ...params } = ctx.query;
+    const results = await find(Organization)
+      .build(params)
+      .count();
+    if (1 === results.length) {
+      ctx.body = { count: results[0]['count(*)'] };
+    } else {
+      ctx.body = { count: 0 };
+    }
   });
 
-  api.get('/organizations/count', async ctx => {
-    const results = await Organization.query().count();
-    ctx.body = { count: results[0]['count(*)'] };
+  /**
+   * GET /organizations/:id
+   */
+  api.get('/organizations/:id', async ctx => {
+    const { select } = ctx.query;
+    const results = await Organization
+      .query()
+      .where('id', '=', ctx.params.id)
+      .skipUndefined()
+      .select(fields(select));
+    if (1 === results.length) {
+      ctx.body = results[0];
+    } 
   });
 
   api.get('/protected', async ctx => {
