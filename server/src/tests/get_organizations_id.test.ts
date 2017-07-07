28ddc2861f82d6d9e1188bc8526ba47beb0aba52
Johannes Hilden <johannes@isomorphic.co>
2017-07-07 15:29:47 +0300

    Fix some typos

--- server/src/tests/get_organizations_id.test.ts
+++ server/src/tests/get_organizations_id.test.ts
@@ -30,13 +30,13 @@ Api.endpoint('/organizations/3?select=name').get(test => {
 
   json(test);
 
-  the(test, 'should return a single organization with a anme property', response => {
+  the(test, 'should return a single organization with a name property', response => {
     response.body
       .should.have.a.property('name')
       .that.is.a('string');
   });
 
-  the(test, 'should return an object without an id property', response => {
+  the(test, 'should not return an object with the id property', response => {
     response.body.should.not.have.a.property('id');
   });
 
