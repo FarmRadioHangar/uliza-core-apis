41d73cb6c45686641eb6ad566a20501deee891cc
Johannes Hilden <johannes@isomorphic.co>
2017-07-08 00:23:31 +0300

    Simplify router code

--- server/src/router.ts
+++ server/src/router.ts
@@ -17,198 +17,198 @@ export default (api: Router) => {
   /**
    * TopicsController
    */
-  const topicsController = new TopicsController();
+  const topics = new TopicsController();
 
   /**
    * List topics.
    */
-  api.get('/topics', topicsController.collection);
+  api.get('/topics', topics.collection);
 
   /**
    * Count the number of topics matching certain criteria.
    */
-  api.get('/topics/count', topicsController.count);
+  api.get('/topics/count', topics.count);
 
   /**
    * Get detailed information about a topic.
    */
-  api.get('/topics/:id', topicsController.findOne);
+  api.get('/topics/:id', topics.findOne);
 
   // -------------------------------------------------------------------------- 
 
   /**
    * SurveysController
    */
-  const surveysController = new SurveysController();
+  const surveys = new SurveysController();
 
   /**
    * List surveys.
    */
-  api.get('/surveys', surveysController.collection);
+  api.get('/surveys', surveys.collection);
 
   /**
    * Count the number of surveys matching certain criteria.
    */
-  api.get('/surveys/count', surveysController.count);
+  api.get('/surveys/count', surveys.count);
 
   /**
    * Get detailed information about a survey.
    */
-  api.get('/surveys/:id', surveysController.findOne);
+  api.get('/surveys/:id', surveys.findOne);
 
   // -------------------------------------------------------------------------- 
 
   /**
    * SchedulesController
    */
-  const schedulesController = new SchedulesController();
+  const schedules = new SchedulesController();
 
   /**
    * List schedules.
    */
-  api.get('/schedules', schedulesController.collection);
+  api.get('/schedules', schedules.collection);
 
   /**
    * Count the number of schedules matching certain criteria.
    */
-  api.get('/schedules/count', schedulesController.count);
+  api.get('/schedules/count', schedules.count);
 
   /**
    * Get detailed information about a schedule.
    */
-  api.get('/schedules/:id', schedulesController.findOne);
+  api.get('/schedules/:id', schedules.findOne);
 
   // -------------------------------------------------------------------------- 
 
   /**
    * ProjectsController
    */
-  const projectsController = new ProjectsController();
+  const projects = new ProjectsController();
 
   /**
    * List projects.
    */
-  api.get('/projects', projectsController.collection);
+  api.get('/projects', projects.collection);
 
   /**
    * Count the number of projects matching certain criteria.
    */
-  api.get('/projects/count', projectsController.count);
+  api.get('/projects/count', projects.count);
 
   /**
    * Get detailed information about a project.
    */
-  api.get('/projects/:id', projectsController.findOne);
+  api.get('/projects/:id', projects.findOne);
 
   // -------------------------------------------------------------------------- 
 
   /**
    * ProgramsController
    */
-  const programsController = new ProgramsController();
+  const programs = new ProgramsController();
 
   /**
    * List programs.
    */
-  api.get('/programs', programsController.collection);
+  api.get('/programs', programs.collection);
 
   /**
    * Count the number of programs matching certain criteria.
    */
-  api.get('/programs/count', programsController.count);
+  api.get('/programs/count', programs.count);
 
   /**
    * Get detailed information about a program.
    */
-  api.get('/programs/:id', programsController.findOne);
+  api.get('/programs/:id', programs.findOne);
 
   // -------------------------------------------------------------------------- 
 
   /**
    * EpisodesController
    */
-  const episodesController = new EpisodesController();
+  const episodes = new EpisodesController();
 
   /**
    * List episodes.
    */
-  api.get('/episodes', episodesController.collection);
+  api.get('/episodes', episodes.collection);
 
   /**
    * Count the number of episodes matching certain criteria.
    */
-  api.get('/episodes/count', episodesController.count);
+  api.get('/episodes/count', episodes.count);
 
   /**
    * Get detailed information about an episode.
    */
-  api.get('/episodes/:id', episodesController.findOne);
+  api.get('/episodes/:id', episodes.findOne);
 
   // -------------------------------------------------------------------------- 
 
   /**
    * CampaignsController
    */
-  const campaignsController = new CampaignsController();
+  const campaigns = new CampaignsController();
 
   /**
    * List campaigns.
    */
-  api.get('/campaigns', campaignsController.collection);
+  api.get('/campaigns', campaigns.collection);
 
   /**
    * Count the number of campaigns matching certain criteria.
    */
-  api.get('/campaigns/count', campaignsController.count);
+  api.get('/campaigns/count', campaigns.count);
 
   /**
    * Get detailed information about a campaign.
    */
-  api.get('/campaigns/:id', campaignsController.findOne);
+  api.get('/campaigns/:id', campaigns.findOne);
 
   // -------------------------------------------------------------------------- 
 
   /**
    * CountriesController
    */
-  const countriesController = new CountriesController();
+  const countries = new CountriesController();
 
   /**
    * List countries.
    */
-  api.get('/countries', countriesController.collection);
+  api.get('/countries', countries.collection);
 
   /**
    * Count the number of countries matching certain criteria.
    */
-  api.get('/countries/count', countriesController.count);
+  api.get('/countries/count', countries.count);
 
   /**
    * Get detailed information about a country.
    */
-  api.get('/countries/:id', countriesController.findOne);
+  api.get('/countries/:id', countries.findOne);
 
   // -------------------------------------------------------------------------- 
 
   /**
    * OrganizationsController
    */
-  const organizationsController = new OrganizationsController();
+  const organizations = new OrganizationsController();
 
   /**
    * List organizations.
    */
-  api.get('/organizations', organizationsController.collection);
+  api.get('/organizations', organizations.collection);
 
   /**
    * Count the number of organizations matching certain criteria.
    */
-  api.get('/organizations/count', organizationsController.count);
+  api.get('/organizations/count', organizations.count);
 
   /**
    * Get detailed information about an organization.
    */
-  api.get('/organizations/:id', organizationsController.findOne);
+  api.get('/organizations/:id', organizations.findOne);
 
   // -------------------------------------------------------------------------- 
 
