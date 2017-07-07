ef1dd4c5d2b152087a81e6282554ebd967acd73d
Johannes Hilden <johannes@isomorphic.co>
2017-07-07 13:22:07 +0300

    Add iso_code column to countries

--- server/migrations/20170706182144_create_countries.js
+++ server/migrations/20170706182144_create_countries.js
@@ -4,10 +4,10 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('countries', function(table) {
     table.increments();
     table.string('name').notNullable();
+    table.string('iso_code').notNullable();
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
   });
-  
 };
 
 exports.down = function(knex, Promise) {
