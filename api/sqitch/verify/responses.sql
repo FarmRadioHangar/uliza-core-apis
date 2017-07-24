c75b839acf349edf4e0f1879f6d3d242e54db521
johanneshilden <hildenjohannes@gmail.com>
2017-07-24 18:22:54 +0300

    Normalize response data

--- api/sqitch/verify/responses.sql
+++ api/sqitch/verify/responses.sql
@@ -1,6 +1,20 @@
 BEGIN;
 
-  SELECT id, data 
+  SELECT 
+      id,               
+      question_id,
+      survey_id,       
+      voto_id,         
+      response_type,   
+      content_type,    
+      poll_id,         
+      delivery_log_id, 
+      choice_id,       
+      subscriber_id,   
+      subscriber_phone,
+      question_title,  
+      choice_name,     
+      date_received     
     FROM farmradio_api.responses
   WHERE FALSE;
 
