b2b72197c6c419576b8070b3d9f6a20a280370a5
Johannes Hilden <johannes@isomorphic.co>
2017-07-25 08:34:21 +0300

    Fix alignment

--- api/sqitch/deploy/responses.sql
+++ api/sqitch/deploy/responses.sql
@@ -1,20 +1,20 @@
 BEGIN;
 
   CREATE TABLE farmradio_api.responses (
-    id               SERIAL      PRIMARY KEY,
-    question_id      INT         NOT NULL,
-    survey_id        INT         NOT NULL,
-    voto_id          INT         NOT NULL,
-    response_type    INT         NOT NULL,
-    content_type     INT         NOT NULL,
-    poll_id          INT         NOT NULL,
-    delivery_log_id  INT         NOT NULL,
-    choice_id        INT         NOT NULL,
-    subscriber_id    INT         NOT NULL,
-    subscriber_phone VARCHAR     NOT NULL,
-    question_title   TEXT        NOT NULL,
-    choice_name      TEXT        NOT NULL,
-    date_received    TIMESTAMPTZ NOT NULL
+    id                SERIAL      PRIMARY KEY,
+    question_id       INT         NOT NULL,
+    survey_id         INT         NOT NULL,
+    voto_id           INT         NOT NULL,
+    response_type     INT         NOT NULL,
+    content_type      INT         NOT NULL,
+    poll_id           INT         NOT NULL,
+    delivery_log_id   INT         NOT NULL,
+    choice_id         INT         NOT NULL,
+    subscriber_id     INT         NOT NULL,
+    subscriber_phone  VARCHAR     NOT NULL,
+    question_title    TEXT        NOT NULL,
+    choice_name       TEXT        NOT NULL,
+    date_received     TIMESTAMPTZ NOT NULL
   );
 
   GRANT SELECT ON farmradio_api.responses TO www;
