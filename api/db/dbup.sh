87c109e1336d2ef12ec874d3167f8a1955bf9b07
Johannes Hilden <johannes@isomorphic.co>
2017-08-09 08:42:27 +0300

    Delete no longer used files

--- api/db/dbup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-(cd /home/db && npm run migrations up)
--- api/db/migrations/1501951865769_create_schema.js
+++
@@ -1,7 +0,0 @@
-exports.up = (pg) => {
-  pg.sql('CREATE SCHEMA farmradio_api;');
-};
-
-exports.down = (pg) => {
-  pg.sql('DROP SCHEMA farmradio_api CASCADE;');
-};
