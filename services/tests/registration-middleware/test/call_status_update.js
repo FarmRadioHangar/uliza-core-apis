b55504a74d1fd84d3ca1ea466d65f7ec3564a96a
Johannes Hilden <johannes@isomorphic.co>
2017-08-13 15:07:34 +0300

    Add test for non-string JSON values

--- services/tests/registration-middleware/test/call_status_update.js
+++ services/tests/registration-middleware/test/call_status_update.js
@@ -2,6 +2,17 @@ var util  = require('util');
 var tests = require('./utils');
 
 var call_status_update_001 = {
+  subscriber_id: "1",
+  subscriber_phone: "255678647268",
+  delivery_status: "6",
+  registered: "true"
+};
+
+var call_status_update_002 = {
+  subscriber_id: 1,
+  subscriber_phone: "255678647268",
+  delivery_status: 6,
+  registered: true
 };
 
 function runner(data, next) {
@@ -31,5 +42,11 @@ describe('Call status update request', function() {
     });
   });
 
-});
+  it('should accept request objects with non-string values', function() {
+    return runner(call_status_update_001, function(response) {
+      response.should.have.header('Content-Type', /json/);
+      response.status.should.equal(200);
+    });
+  });
 
+});
