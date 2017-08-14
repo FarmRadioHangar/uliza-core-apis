1a2347e5984942dd5692c9a7c5099839c46391c1
Johannes Hilden <johannes@isomorphic.co>
2017-08-14 08:11:55 +0300

    Implement code to process call status update

--- services/registration-middleware/src/FarmRadio/Uliza/Registration.hs
+++ services/registration-middleware/src/FarmRadio/Uliza/Registration.hs
@@ -9,6 +9,7 @@ import Control.Monad.IO.Class
 import Control.Monad.Trans.Either
 import Data.Aeson
 import Data.Either.Utils  ( maybeToEither )
+import Data.Maybe         ( isJust, fromJust )
 import Data.Monoid        ( (<>) )
 import Data.Text
 import Data.Text.Encoding ( encodeUtf8 )
@@ -41,8 +42,7 @@ patchParticipant = fmap fmap fmap void (patchResource "participants")
 
 -- | Post a participant to the API.
 postParticipant :: Text -> Api (Maybe Participant)
-postParticipant phone = do
-    post_ "/participants" (object participant)
+postParticipant phone = post_ "/participants" (object participant)
   where
     participant = [ ("phone_number"        , String phone)
                   , ("registration_status" , "NOT_REGISTERED") ]
@@ -53,61 +53,67 @@ registrationCallScheduleTime RegistrationCall{..} =
     eitherToMaybe $ parseUTCTime (encodeUtf8 scheduledTime)
 
 -- | Post a registration call status change log entry to the API.
-createStatusChangeLogEntry :: Int -> Int -> Text -> Api () 
+createStatusChangeLogEntry :: Int -> Maybe Int -> Text -> Api () 
 createStatusChangeLogEntry user call event = 
     void $ post "/participant_registration_status_log" (object entry)
   where
     entry = [ ("participant_id"       , number user) 
-            , ("registration_call_id" , number call) 
+            , ("registration_call_id" , maybe Null number call) 
             , ("event_type"           , String event) ]
 
--- | Post a registration call to the API.
+-- | Post registration call to the API.
 postRegistrationCall :: Text -> Text -> Api (Maybe RegistrationCall)
 postRegistrationCall phone stime = post_ "/registration_calls" (object call)
   where
     call = [ ("phone_number"   , String phone)
            , ("scheduled_time" , String stime) ]
 
--- | Process request body and lookup or create a participant based on the
---   provided subscriber_phone property.
-lookupParticipant :: Value -> Api Participant
-lookupParticipant request = do
+-- | Look up a participant 
+data LookUpParticipant
+   = FromRequest Value      -- ^ from request object (JSON)
+   | FromPhoneNumber Text   -- ^ from phone number
+  deriving (Show)
+
+-- | Look up or create a participant from the request object or a phone number.
+getOrCreateParticipant :: LookUpParticipant -> Api Participant
+getOrCreateParticipant (FromRequest request) = do
 
     -- Extract phone number from request 
     phone <- maybeToEither BadRequestError (extractString "subscriber_phone" request) 
 
-    logDebugJSON "incoming_response" request
+    getOrCreateParticipant (FromPhoneNumber phone)
 
-    -- Log the raw JSON payload received
-    void $ post "/voto_response_data" $ object [("data", request)] 
+getOrCreateParticipant (FromPhoneNumber phone)  = do
+
+    -- Strip + prefix
+    let phone' = if '+' == Data.Text.head phone then Data.Text.tail phone
+                                                else phone 
 
     -- Look up participant from subscriber's phone number
-    response <- getParticipantByPhoneNumber (unpack phone)
+    response <- getParticipantByPhoneNumber (unpack phone')
 
     logDebugJSON "lookup_participant" response
 
     case response of
+      --
+      -- Participant exists: Done!  
+      --
       Just participant -> do
-          --
-          -- Participant exists: Done!  
-          --
+
           logDebugJSON "participant_found" participant
-
           right participant
-
+      --
+      -- Create a participant if one wasn't found
+      --
       Nothing -> do
-          --
-          -- Create a participant if one wasn't found
-          --
-          participant <- postParticipant phone 
 
+          participant <- postParticipant phone'
           logDebugJSON "participant_created" participant
-
           maybeToEither (UnexpectedResponse "postParticipant") participant
 
 -- | Data type representation of a participant's registration status
-data RegistrationStatus = 
-     AlreadyRegistered       -- ^ The participant is already registered
+data RegistrationStatus 
+   = AlreadyRegistered       -- ^ The participant is already registered
    | RegistrationDeclined    -- ^ Participant has declined to register
    | PriorCallScheduled      -- ^ A registration call is already due
    | RecentCallMade          -- ^ A call was recently made
@@ -156,7 +162,7 @@ determineRegistrationStatus Participant{..} mcall = do
 scheduleRegistrationCall :: Participant -> UTCTime -> Api RegistrationCall
 scheduleRegistrationCall Participant{ entityId = participantId, .. } time = do
 
-    -- Send registration call to API for posterity
+    -- Post registration call to API 
     regc <- postRegistrationCall phoneNumber (utcToText time)
             >>= maybeToEither (UnexpectedResponse "postRegistrationCall")
 
@@ -174,9 +180,33 @@ scheduleRegistrationCall Participant{ entityId = participantId, .. } time = do
     logNoticeJSON "patch_participant" ("update registration_call_id to " <> show call)
 
     -- Create a log entry to record the status change 
-    createStatusChangeLogEntry user call "REGISTRATION_CALL_SCHEDULED"
+    createStatusChangeLogEntry user (Just call) "REGISTRATION_CALL_SCHEDULED"
 
     logNoticeJSON "registration_call_scheduled" regc
 
     return regc
 
+registerParticipant :: Participant -> Api Value
+registerParticipant participant@Participant{ entityId = participantId, .. } = do
+
+    user <- maybeToEither (UnexpectedResponse "registerParticipant: \
+                          \participant id is null") participantId
+
+    when ("REGISTERED" == registrationStatus) $ logWarning "already_registered" 
+          "Registration for already registered listener."
+
+    -- Update the participant's registration_status
+    patchParticipant user $ object [("registration_status", "REGISTERED")]
+
+    -- Create a log entry to record the status change 
+    createStatusChangeLogEntry user registrationCallId "REGISTRATION_COMPLETE"
+
+    return (toJSON participant { registrationStatus = "REGISTERED" })
+
+isCallComplete :: Value -> Api Bool
+isCallComplete request = do
+
+    -- Extract delivery status
+    status <- maybeToEither BadRequestError (extractInt "delivery_status" request) 
+
+    return (6 == status)
