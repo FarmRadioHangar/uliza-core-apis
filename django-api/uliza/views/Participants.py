76dc34143afe2ab868c9aa99bd104828b28d3f0c
johanneshilden <hildenjohannes@gmail.com>
2017-08-18 18:38:20 +0300

    Overrride perform_update in Participant view

--- django-api/uliza/views/Participants.py
+++ django-api/uliza/views/Participants.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from uliza.models import Participant
+from uliza.models import Participant, ParticipantRegistrationStatusLog
 from uliza.serializers import ParticipantSerializer
 
 class Participants(generics.ListCreateAPIView):
@@ -15,3 +15,25 @@ class ParticipantsInstance(generics.RetrieveUpdateAPIView):
     model = Participant
     serializer_class = ParticipantSerializer
     lookup_field = 'id'
+
+    def perform_update(self, serializer):
+        pk = self.kwargs['id']
+        if pk and 'PATCH' == self.request.method and 'registration_status' in self.request.data:
+            participant = Participant.objects.all().get(pk=pk)
+            if 'NOT_REGISTERED' == participant.registration_status:
+                event_type = self.event_type(serializer.validated_data['registration_status'])
+                log_entry = ParticipantRegistrationStatusLog(
+                        registration_call=participant.registration_call,
+                        participant=participant, 
+                        event_type=event_type)
+                log_entry.save()
+        serializer.save()
+
+    def event_type(self, status):
+        if status == 'REGISTERED':
+            return 'REGISTRATION_COMPLETE'
+        elif status == 'DECLINED':
+            return 'REGISTRATION_DECLINED'
+        return None
+
+
