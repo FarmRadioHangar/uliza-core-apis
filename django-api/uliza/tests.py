87aed2a3eff1987e4052cd060c0ea98cb58d8327
johanneshilden <hildenjohannes@gmail.com>
2017-08-23 12:20:04 +0300

    Use response.data directly instead of json parser

--- django-api/uliza/tests.py
+++ django-api/uliza/tests.py
@@ -12,8 +12,7 @@ class GetParticipantsTests(TestCase):
         client = Client()
         response = client.get('/api/v1/participants/1')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        #import pdb; pdb.set_trace()
-        self.assertTrue(re.compile(r'json').search(response._headers['content-type']))
-        json_body = json.loads(response.content)
-        self.assertEqual(json_body.get('phone_number'), '255678647268')
+        self.assertTrue(re.compile(r'json').search(response['Content-Type']))
+        self.assertEqual(response.data.get('phone_number'), '255678647268')
 
+        #import pdb; pdb.set_trace()
