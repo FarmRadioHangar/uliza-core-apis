d85214f70a7eb37385edc8a7ea586beb9cd97b49
Jigsa Tesfaye <jiks.tes@gmail.com>
2017-08-30 11:59:02 +0300

    ordering backend for log model

--- django-api/log_app/views/vLogs.py
+++ django-api/log_app/views/vLogs.py
@@ -6,12 +6,31 @@ from rest_framework.views import APIView
 from log_app.models import Log
 from log_app.serializers import LogSerializer
 
+from rest_framework import filters
+
 class LogGet(generics.ListCreateAPIView):
 
-    queryset = Log.objects.all()
-    model = Log
-    serializer_class = LogSerializer
-    filter_fields = ['id','program','program__radio_station__country']
+	queryset = Log.objects.all()
+	model = Log
+	serializer_class = LogSerializer
+	filter_fields = ['id','program','program__radio_station__country','postpone']
+	filter_backends = (filters.OrderingFilter,)
+	ordering_fields = ('week','id')
+
+	def get_queryset(self):
+		"""
+		This view should return a list of all the purchases
+		for the currently authenticated user.
+		"""
+		pk_list = self.request.GET.get('program__in')
+		if pk_list:
+			pk_list = pk_list.split(',')
+			logs = Log.objects.filter(program__in=pk_list)
+		else:
+			logs = Log.objects.all()
+
+		return logs
+
 
 class LogEntity(generics.RetrieveUpdateAPIView):
 
