a4dec1519471bf16ad9bc2e322895a1bad9ae462
johanneshilden <hildenjohannes@gmail.com>
2017-08-31 16:03:46 +0300

    Fix Docker setup in tests

--- services/tests/registration-middleware/test/e2e.js
+++ services/tests/registration-middleware/test/e2e.js
@@ -17,8 +17,22 @@ chai.use(require('chai-http'));
 
 var docker = new Docker();
 
+function createMiddlewareContainer() {
+  console.log('Create middleware container');
+  return docker.createContainer({
+    'name': 'middleware',
+    'Image': 'registration_middleware',
+    'ExposedPorts': { '3034': {} },
+    'HostConfig': {
+      'Links': ['api'],
+      'PortBindings': { '3034': [{'HostPort': '3034'}] }
+    }
+  });
+}
+
 function createDatabaseContainer() {
-  var options = {
+  console.log('Create mysql container');
+  return docker.createContainer({
     'name': 'database',
     'Image': 'mysql',
     'ExposedPorts': { '3306': {} },
@@ -30,17 +44,19 @@ function createDatabaseContainer() {
     'AttachStdout': true,
     'AttachStderr': true,
     'Tty': true
-  };
-  return docker.createContainer(options);
+  });
 }
 
 function createApiContainer() {
-  var options = {
+  console.log('Create api container');
+  return docker.createContainer({
     'name': 'api',
     'Image': 'django_api',
     'Cmd': [ 'python', 'manage.py', 'runserver', '0.0.0.0:8000' ],
+    'ExposedPorts': { '8000': {} },
     'HostConfig': {
-      'Links': ['database']
+      'Links': ['database'],
+      'PortBindings': { '8000': [{'HostPort': '8000'}] }
     },
     'Env': [
       'DEBUG="true"',
@@ -49,43 +65,49 @@ function createApiContainer() {
       'DATABASE_PASSWORD=root',
       'DATABASE_SERVICE_HOST=database'
     ]
-  };
-  return docker.createContainer(options);
+  });
 }
 
-var buildArchive = './.build/django_api.tar.gz';
-
-function createTarArchive() {
-  if (fs.existsSync(buildArchive)) {
-    console.log('Archive found.');
-    return Promise.resolve();
-  } else {
-    console.log('Creating django-api tar archive.');
-    return targz({}, {fromBase: true}).compress('../../../django-api/', buildArchive); 
+function createArchive(path, archive) {
+  return function() {
+    return new Promise(function(resolve, reject) {
+      if (fs.existsSync(archive)) {
+        console.log('Archive found: ' + archive);
+        resolve(archive);
+      } else {
+        console.log('Creating django-api tar archive: ' + archive);
+        targz({}, {fromBase: true}).compress(path, archive).then(function() { 
+          resolve(archive); 
+        }); 
+      }
+    });
   }
 }
 
-function buildApiImage() {
-  return new Promise(function(resolve, reject) {
-    var EchoStream = function() { 
-      stream.Writable.call(this); 
-    };
-    util.inherits(EchoStream, stream.Writable); 
-    EchoStream.prototype._write = function(chunk, encoding, done) { 
-      process.stdout.write(JSON.parse(chunk.toString()).stream);
-      done();
-    }
-    docker.buildImage(buildArchive, {t: 'django_api'}, function(err, stream) {
-     if (err) {
-        reject(err);
+function buildImage(tag) {
+  return function(archive) {
+    return new Promise(function(resolve, reject) {
+      var EchoStream = function() { 
+        stream.Writable.call(this); 
+      };
+      util.inherits(EchoStream, stream.Writable); 
+      EchoStream.prototype._write = function(chunk, encoding, done) { 
+        process.stdout.write(JSON.parse(chunk.toString()).stream);
+        done();
       }
-      var writeStream = new EchoStream(); 
-      stream.pipe(writeStream, { 
-        end: true 
+      console.log('Build image ' + tag + ' from archive ' + archive);
+      docker.buildImage(archive, {t: tag}, function(err, stream) {
+       if (err) {
+          reject(err);
+        }
+        var writeStream = new EchoStream(); 
+        stream.pipe(writeStream, { 
+          end: true 
+        });
+        stream.on('end', resolve);
       });
-      stream.on('end', resolve);
     });
-  });
+  }
 }
 
 describe('123', function() {
@@ -101,9 +123,10 @@ describe('123', function() {
     }
   };
 
-  var startContainer = function(name) {
+  var startContainer = function(name, opts) {
     return function() {
-      return self._containers[name].start(); 
+      console.log('Starting container ' + name);
+      return self._containers[name].start(opts); 
     }
   };
 
@@ -119,14 +142,45 @@ describe('123', function() {
     }
   };
 
-  var makeMigrations = function() { 
-    return exec(['python', 'manage.py', 'makemigrations']); 
+  var runExec = function(container) {
+    var cmd = Array.from(arguments).slice(1);
+    return new Promise(function(resolve, reject) {
+      container.execRaw({
+        Cmd: cmd, 
+        AttachStdin: true, 
+        AttachStdout: true,
+        tty: true
+      }, function(err, exec) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        exec.start({
+          hijack: true, 
+          stdin: true
+        }, function(err, stream) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          container.modem.demuxStream(stream, process.stdout, process.stderr);
+          process.stdin.pipe(stream);
+          bufs = [];
+          stream.on('end', resolve);
+        });
+      });
+    });
   };
 
-  var exec = function(cmd, opts) {
-    var defaults = {live: true, stdout: true, stderr: true};
+  var makeMigrations = function() {
+    return runExec(self._containers.api, 'python', 'manage.py', 'makemigrations');
+  };
+
+  var exec = function() {
+    var cmd = Array.from(arguments);
     return new Promise(function(resolve, reject) {
-      self._containers.api.exec(cmd, opts || defaults, function(err, streamLink) {
+      self._containers.api.exec(cmd, {live: true, stdout: true, stderr: true}, 
+      function(err, streamLink) {
         if (err) {
           return reject(err);
         }
@@ -151,6 +205,7 @@ describe('123', function() {
             retry();
           } else {
             console.log('.');
+            console.log('Database server is accepting connections.');
             resolve();
           }
         });
@@ -160,37 +215,41 @@ describe('123', function() {
   };
 
   before(function() { 
-    return createTarArchive()
-    .then(buildApiImage)
+    return new Promise(function(resolve, reject) { resolve(); })
+    .then(createArchive('../../../django-api/', './.build/django_api.tar.gz'))
+    .then(buildImage('django_api'))
+    .then(createArchive('../../registration-middleware/', './.build/middleware.tar.gz'))
+    .then(buildImage('registration_middleware'))
     .then(createDatabaseContainer)
     .then(saveContainer('db'))
     .then(createApiContainer)
     .then(saveContainer('api'))
+    .then(createMiddlewareContainer)
+    .then(saveContainer('middleware'))
     .then(startContainer('db'))
     .then(startContainer('api'))
+    .then(startContainer('middleware' ))
     .then(pingMysql)                     // Wait for MySQL to accept connections
     .then(makeMigrations)
-    .catch(function(err) { 
-      console.error(err); 
-    });
+    .catch(console.error);
   });
 
   after(function() { 
     return stopContainer('db')()
     .then(removeContainer('db'))
+    .then(stopContainer('middleware'))
+    .then(removeContainer('middleware'))
     .then(stopContainer('api'))
     .then(removeContainer('api'))
-    .catch(function(err) { 
-      console.error(err); 
-    });
+    .catch(console.error);
   });
 
   beforeEach(function() { 
-    return exec(['python', 'manage.py', 'migrate']);
+    return exec('python', 'manage.py', 'migrate');
   });
 
   afterEach(function() { 
-    return exec(['python', 'manage.py', 'migrate', 'uliza', 'zero']);
+    return exec('python', 'manage.py', 'migrate', 'uliza', 'zero');
   });
 
   it('should be ok', function() {});
