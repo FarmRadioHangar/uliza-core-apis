5d7849bb8f27c6abd5c79e25a0c1f86b6e5fe93f
Johannes Hilden <hildenjohannes@gmail.com>
2017-10-28 16:25:21 +0300

    Log incoming_call_status_update

--- services/registration-middleware/src/FarmRadio/Uliza/Registration/Voto/CallStatusUpdateHandler.hs
+++ services/registration-middleware/src/FarmRadio/Uliza/Registration/Voto/CallStatusUpdateHandler.hs
@@ -2,8 +2,20 @@ module FarmRadio.Uliza.Registration.Voto.CallStatusUpdateHandler
   ( votoCallStatusUpdate
   ) where
 
+import Control.Lens
+import Control.Monad.IO.Class
+import Control.Monad.State
 import Data.Aeson
 import FarmRadio.Uliza.Registration
+import FarmRadio.Uliza.Registration.Logger
+
+import qualified Data.ByteString.Lazy.Char8           as B8
 
 votoCallStatusUpdate :: RegistrationHandler Value
-votoCallStatusUpdate = return $ toJSON $ object []
+votoCallStatusUpdate = do
+    state <- get
+    let body = state ^. requestBody
+    -- Log raw VOTO webhook request object
+    logDebug "incoming_incoming_call_status_update" (B8.unpack body) & liftIO
+
+    return $ toJSON $ object []
