68f355a71f70d9825d45c6761aab965be6d64d45
Johannes Hilden <hildenjohannes@gmail.com>
2017-10-28 13:30:21 +0300

    Create repl utility script

---
+++ services/tests/registration-middleware/repl-utility.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+
+var request = require('request');
+var qs      = require('qs');
+
+function serialize(obj) {
+  var str = [];
+  for (var p in obj) {
+    if (obj.hasOwnProperty(p)) {
+      str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
+    }
+  }
+  return str.join('&');
+}
+
+var testdata_0 = {
+  question_id: "127375", 
+  survey_id: "89324", 
+  voto_id: "44", 
+  response_type: "1", 
+  content_type: "1", 
+  poll_id: "213", 
+  delivery_log_id: "832", 
+  choice_id: "1", 
+  subscriber_id: "232", 
+  subscriber_phone: "+255678647268", 
+  question_title: "Who was the first man to set foot on the moon?", 
+  choice_name: "Neil Armstrong", 
+  date_received: "2017-07-24T18:13:51Z"
+};
+
+var responses = function(data) {
+  request.post({
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Accept': 'application/json'
+    },
+    url: process.env.REG_SERVICE_URL + '/responses',
+    body: serialize(data)
+  }, function(error, response, body) {
+    console.log(body);
+  });
+}
+
+module.exports = {
+  responses: responses,
+  testdata_0: testdata_0
+};
