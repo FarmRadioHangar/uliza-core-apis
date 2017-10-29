5ca95dcc4c93dd33cb2c90bc5170c6544bdd761e
Johannes Hilden <hildenjohannes@gmail.com>
2017-10-29 12:03:23 +0300

    Send log data to Uliza API

--- services/registration-middleware/src/FarmRadio/Uliza/Registration/Voto/CallStatusUpdateHandler.hs
+++ services/registration-middleware/src/FarmRadio/Uliza/Registration/Voto/CallStatusUpdateHandler.hs
@@ -11,11 +11,15 @@ import FarmRadio.Uliza.Registration.Logger
 
 import qualified Data.ByteString.Lazy.Char8           as B8
 
+-- | Response handler for VOTO call status update webhook.
 votoCallStatusUpdate :: RegistrationHandler Value
 votoCallStatusUpdate = do
     state <- get
     let body = state ^. requestBody
     -- Log raw VOTO webhook request object
     logDebug "incoming_call_status_update" (B8.unpack body) & liftIO
+    ulizaApiPost_ "/voto_webhook_log" $ object
+      [ ("data"     , String (toText body))
+      , ("endpoint" , "call_status_updates") ]
 
     return $ toJSON $ object []
