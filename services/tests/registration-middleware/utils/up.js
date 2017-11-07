114631713db8ae00cb9680e3a1a125e74deb789c
Johannes Hilden <hildenjohannes@gmail.com>
2017-11-07 15:34:04 +0300

    Update Docker build step

--- services/tests/registration-middleware/utils/up.js
+++ services/tests/registration-middleware/utils/up.js
@@ -195,19 +195,18 @@ function up() {
   return Promise.resolve()
   .then(acquireBaseImages)
   .then(buildImage('django_api', '../../../django-api/'))
-  .then(buildImage('xxx', './'))
-  //.then(buildImage('registration_middleware', '../../registration-middleware/'))
-  //.then(createContainer('mysql'))
-  //.then(createContainer('api'))
-  //.then(createContainer('middleware'))
-  //.then(startContainer('database'))
-  //.then(startContainer('api'))
-  //// Wait for MySQL to accept connections
-  //.then(pingMysql)                     
-  //.then(startContainer('middleware' ))
-  //.then(runMigrations)
-  //.then(runServer)
-  //.then(pingApi) 
+  .then(buildImage('registration_middleware', '../../registration-middleware/'))
+  .then(createContainer('mysql'))
+  .then(createContainer('api'))
+  .then(createContainer('middleware'))
+  .then(startContainer('database'))
+  .then(startContainer('api'))
+  // Wait for MySQL to accept connections
+  .then(pingMysql)                     
+  .then(startContainer('middleware' ))
+  .then(runMigrations)
+  .then(runServer)
+  .then(pingApi) 
   .catch(console.error);
 }
 
