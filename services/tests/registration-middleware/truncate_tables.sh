94f5aab1217d40760b1dde3629558fdf37b85c37
Johannes Hilden <hildenjohannes@gmail.com>
2017-11-09 15:32:48 +0300

    Add helper script to flush db between tests

---
+++ services/tests/registration-middleware/truncate_tables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+query="SET FOREIGN_KEY_CHECKS=0; \
+       TRUNCATE TABLE uliza_participant_registration_status_log; \
+       TRUNCATE TABLE uliza_voto_webhook_log; \
+       TRUNCATE TABLE uliza_participants; \
+       TRUNCATE TABLE uliza_registration_calls; \
+       TRUNCATE TABLE eav_attribute; \
+       TRUNCATE TABLE eav_enumgroup; \
+       TRUNCATE TABLE eav_enumgroup_enums; \
+       TRUNCATE TABLE eav_enumvalue; \
+       TRUNCATE TABLE eav_value; \
+       SET FOREIGN_KEY_CHECKS=1;"
+
+docker exec -it database mysql -uroot -proot -e "$query" api_core
