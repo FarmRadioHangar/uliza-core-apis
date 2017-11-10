73c2f4a19f7893a4e3421fa690835c125d710f92
Johannes Hilden <hildenjohannes@gmail.com>
2017-11-10 16:15:24 +0300

    Log all responses in debug mode

--- services/registration-middleware/src/FarmRadio/Uliza/Api/RegistrationCall.hs
+++ services/registration-middleware/src/FarmRadio/Uliza/Api/RegistrationCall.hs
@@ -14,24 +14,36 @@ import FarmRadio.Uliza.Registration.Logger
 import FarmRadio.Uliza.Registration.Participant
 import FarmRadio.Uliza.Registration.RegistrationCall
 import FarmRadio.Uliza.Registration.Voto.CallScheduleResponse
+import TextShow
 
-scheduleVotoCall :: Text -- ^ Phone number
+-- | Post request to VOTO's API to schedule a registration call.
+scheduleVotoCall :: Text 
+                 -- ^ Phone number
                  -> RegistrationHandler (Maybe CallScheduleResponse)
+                 -- ^ Return the VOTO response
 scheduleVotoCall phone = do
-    votoApiPost "/outgoing_calls" (object call)
+    state <- get
+    let payload = object $ call (state ^. config . votoApiKey & pack)
+    logDebugJSON "voto_call_scheduled" payload & liftIO
+    votoApiPost "/outgoing_calls" payload
   where
-    call = [ ("send_to_phones" , String phone) ]
+    call key = [ ("send_to_phones" , String phone) 
+               , ("tree_id"        , String "19278")    -- @TODO: temp
+               , ("api_key"        , String key) ]
 
-postRegistrationCall :: Text -- ^ Phone number
-                     -> Text -- ^ Schedule time
-                     -> RegistrationHandler (Maybe RegistrationCall)
-postRegistrationCall phone time = ulizaApiPost "/registration_calls" (object call)
+-- | Create the registration call in Uliza.
+createRegistrationCall :: Text -- ^ Phone number
+                       -> Int  -- ^ VOTO call id
+                       -> Text -- ^ Schedule time
+                       -> RegistrationHandler (Maybe RegistrationCall)
+createRegistrationCall phone votoId time = ulizaApiPost "/registration_calls" (object call)
   where
     call = [ ("phone_number"   , String phone)
-           , ("scheduled_time" , String time) ]
+           , ("scheduled_time" , String time) 
+           , ("voto_id"        , String (showt votoId)) ]
 
--- | Request the most recent 'RegistrationCall' for a participant from the
---   Uliza API.
+-- | Request the most recent 'RegistrationCall' for the provided participant 
+--   from the Uliza API.
 getRegistrationCall :: Participant
                     -> RegistrationHandler (Maybe RegistrationCall)
 getRegistrationCall Participant{..} = join <$> sequence call
@@ -42,23 +54,23 @@ getRegistrationCall Participant{..} = join <$> sequence call
 getRegistrationCallById :: Int -> RegistrationHandler (Maybe RegistrationCall)
 getRegistrationCallById = ulizaApiGetOne "/registration_calls" []
 
--- | Schedule a registration call for a participant at a given time.
+-- | Schedule a registration call for a participant at the given time.
 scheduleRegistrationCall :: Participant
                          -- ^ The participant to call
                          -> UTCTime
                          -- ^ Time when the registration call is to be made
                          -> RegistrationHandler RegistrationCall
+                         -- ^ Return the details of the scheduled call
 scheduleRegistrationCall Participant{ entityId = participantId, .. } time = do
     -- Schedule an outgoing call with VOTO
-    response <- scheduleVotoCall phoneNumber
-    maybeToEither votoErr response
-
-    print response & liftIO 
+    scheduleVotoCall phoneNumber >>= maybeToEither votoErr 
     -- Post registration call to Uliza API
-    postRegistrationCall phoneNumber (utcToText time)
+    >>= \CallScheduleResponse{..} -> 
+          createRegistrationCall phoneNumber callId (utcToText time)
     >>= maybeToEither ulizaErr
     >>= \call -> logNoticeJSON "registration_call_scheduled" call & liftIO
      >> return call
   where
     ulizaErr = UlizaApiError "scheduleRegistrationCall: Unexpected response"
-    votoErr  = undefined -- VotoApiError "scheduleRegistrationCall: Unexpected response"
+    votoErr  = UlizaApiError "scheduleRegistrationCall: Unexpected response"
+    -- @TODO: VotoApiError "scheduleRegistrationCall: Unexpected response"
