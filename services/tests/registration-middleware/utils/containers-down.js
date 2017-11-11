1f097c14bf59b38cabb89892074068a43b9f477e
Johannes Hilden <hildenjohannes@gmail.com>
2017-11-11 12:27:17 +0300

    Run mock server for testing webhooks

--- services/tests/registration-middleware/utils/containers-down.js
+++ services/tests/registration-middleware/utils/containers-down.js
@@ -1,6 +1,8 @@
 var $ = require('./docker');
 
 return Promise.resolve()
+.then($.stopContainer('voto'))
+.then($.removeContainer('voto'))
 .then($.stopContainer('middleware'))
 .then($.removeContainer('middleware'))
 .then($.stopContainer('api'))
