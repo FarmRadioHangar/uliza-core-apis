eee30742f8ae0cbb3029daf48236149d09a29f3a
Johannes Hilden <hildenjohannes@gmail.com>
2017-11-14 19:35:15 +0300

    Set lookup_field to voto_survey_id

--- django-api/uliza/views/VotoSurveyRegistrationTree.py
+++ django-api/uliza/views/VotoSurveyRegistrationTree.py
@@ -1,8 +1,4 @@
-from django.http import JsonResponse
 from rest_framework import generics
-from rest_framework import status
-from rest_framework.exceptions import NotFound
-from rest_framework.views import APIView
 from uliza.models import VotoSurveyRegistrationTree
 from uliza.serializers import VotoSurveyRegistrationTreeSerializer
 
@@ -19,4 +15,4 @@ class VotoSurveyRegistrationTreeInstance(generics.RetrieveUpdateAPIView):
     queryset = VotoSurveyRegistrationTree.objects.all()
     model = VotoSurveyRegistrationTree
     serializer_class = VotoSurveyRegistrationTreeSerializer
-    lookup_field = 'survey_id'
+    lookup_field = 'voto_survey_id'
