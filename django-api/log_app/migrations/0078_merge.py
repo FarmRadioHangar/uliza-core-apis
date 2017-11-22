7dcab1a83f64f87f9fcf4a097d9b704898a6e907
Johannes Hilden <hildenjohannes@gmail.com>
2017-11-22 11:41:01 +0300

    Run python manage.py makemigrations --merge

---
+++ django-api/log_app/migrations/0078_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('log_app', '0071_auto_20170901_0923'),
+        ('log_app', '0077_contact_radio_station'),
+    ]
+
+    operations = [
+    ]
