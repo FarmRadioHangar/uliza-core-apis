a1a667c531aa707c82ca1404f0d143601711cfa1
Johannes Hilden <hildenjohannes@gmail.com>
2017-11-25 15:11:38 +0300

    Rename Default to Collection

--- django-api/uliza/views/VotoWebhookLog.py
+++ django-api/uliza/views/VotoWebhookLog.py
@@ -1,16 +1,14 @@
-from django.http import JsonResponse
 from rest_framework import generics
-from rest_framework import status
-from rest_framework.exceptions import NotFound
-from rest_framework.views import APIView
 from uliza.models import VotoWebhookLog
 from uliza.serializers import VotoWebhookLogSerializer
 
-class VotoWebhookLogDefault(generics.ListCreateAPIView):
+
+class VotoWebhookLogCollection(generics.ListCreateAPIView):
 
     queryset = VotoWebhookLog.objects.all()
     model = VotoWebhookLog
     serializer_class = VotoWebhookLogSerializer
+
 
 class VotoWebhookLogInstance(generics.RetrieveUpdateAPIView):
 
