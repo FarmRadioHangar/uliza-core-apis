092ce47d8c6eda8c31ef862511bd168a86bddb7d
Johannes Hilden <hildenjohannes@gmail.com>
2017-11-25 17:27:12 +0300

    Add comments

--- services/tests/registration-middleware/utils/containers.sh
+++ services/tests/registration-middleware/utils/containers.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Remove containers left over from earlier sessions
 docker rm --force $(docker ps -aq --filter "name=ulizatests_*") 2> /dev/null
 
+# Create and run mysql container
 docker run \
   -d \
   -e "MYSQL_DATABASE=api_core" \
@@ -10,6 +12,8 @@ docker run \
   --name ulizatests_db \
   mysql:5.7
 
+# Wait for database server to accept connections
+echo "Waiting for mysql to accept connections"
 until mysqladmin ping \
   -uroot \
   -proot \
@@ -19,12 +23,14 @@ until mysqladmin ping \
         --filter "name=ulizatests_db" | sed -n 's/[^:]*:\([0-9]*\).*/\1/p') \
         2> /dev/null ; do echo -n "." ; sleep 3 ; done
 
+# Create and run mock VOTO API container
 docker run \
   -d \
   -p "0:8089" \
   --name ulizatests_voto \
   farmradio/voto_mock_api
 
+# Create and run Uliza API container
 docker run \
   -d \
   -e "DEBUG=true" \
@@ -39,6 +45,7 @@ docker run \
   --name ulizatests_api \
   farmradio/uliza_api
 
+# Create and run registration service container
 docker run \
   -d \
   -p "0:3034" \
@@ -53,6 +60,7 @@ docker run \
   --name ulizatests_middleware \
   farmradio/registration_service
 
+# Apply database migrations
 docker exec -it ulizatests_api python manage.py migrate
 
 function get_port () {
@@ -61,20 +69,25 @@ function get_port () {
       --filter "name=$1" | sed -n 's/[^:]*:\([0-9]*\).*/\1/p' 
 }
 
+# Create ENV variables for tests
 OUT="\
 REG_SERVICE_URL=http://0.0.0.0:$(get_port ulizatests_middleware)\n\
 DB_HOST=0.0.0.0\n\
 DB_PORT=$(get_port ulizatests_db)\n\
+ULIZA_API_URL=http://0.0.0.0:$(get_port ulizatests_api)/api/v1\n\
 VOTO_API_URL=http://0.0.0.0:$(get_port ulizatests_voto)/api/v1\n\
 VOTO_API_KEY=XXX_TEST_KEY_XXX"
 
 echo -e "---------------------------------------------------\
  \n$OUT\n---------------------------------------------------\n"
 
+# Confirm with user to overwrite .env
 read -r -p "Write above contents to .env? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
     echo -e $OUT > .env
+    echo ".env file updated"
 fi
 
+# List running containers
 docker ps
