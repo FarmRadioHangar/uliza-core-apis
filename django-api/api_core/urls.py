66f528e8ed3f10dd5be6a44879eca746f56bbf6f
Johannes Hilden <hildenjohannes@gmail.com>
2017-11-26 12:34:58 +0300

    Fix indentation

--- django-api/api_core/urls.py
+++ django-api/api_core/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^admin', include(admin.site.urls)),    	
+    url(r'^admin', include(admin.site.urls)),
     url(r'^api/v1/', include('log_app.urls')),
     url(r'^api/v1/', include('uliza.urls')),
 ]
