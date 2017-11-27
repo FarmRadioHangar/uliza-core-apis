9985fcebeeea008e902e6d7366a506764e51e8ba
Johannes Hilden <hildenjohannes@gmail.com>
2017-11-27 14:58:24 +0300

    Change port from 3316 to 3306

--- services/tests/registration-middleware/utils/db-up.sh
+++ services/tests/registration-middleware/utils/db-up.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name database -p3316:3306 -e MYSQL_DATABASE=api_core -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7
+docker run --name database -p3306:3306 -e MYSQL_DATABASE=api_core -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7
