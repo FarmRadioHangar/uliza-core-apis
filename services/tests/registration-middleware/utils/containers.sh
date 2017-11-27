6be27735d6a8505c7fc5e129b968514a3b3f7021
Johannes Hilden <hildenjohannes@gmail.com>
2017-11-27 21:37:37 +0300

    Remove underscores from container names

--- services/tests/registration-middleware/utils/containers.sh
+++ services/tests/registration-middleware/utils/containers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Remove any containers left over from earlier sessions
-docker rm --force $(docker ps -aq --filter "name=ulizatests_*") 2> /dev/null
+docker rm --force $(docker ps -aq --filter "name=ulizatests*") 2> /dev/null
 
 # Create and run mysql container
 docker run \
@@ -9,7 +9,7 @@ docker run \
   -e "MYSQL_DATABASE=uliza_core" \
   -e "MYSQL_ROOT_PASSWORD=root" \
   -p "0:3306" \
-  --name ulizatests_db \
+  --name ulizatestsdb \
   mysql:5.7
 
 # Wait for database server to accept connections
@@ -20,14 +20,14 @@ until mysqladmin ping \
   -h0.0.0.0 \
   -P$(docker ps \
         --format "{{.Ports}}" \
-        --filter "name=ulizatests_db" | sed -n 's/[^:]*:\([0-9]*\).*/\1/p') \
+        --filter "name=ulizatestsdb" | sed -n 's/[^:]*:\([0-9]*\).*/\1/p') \
         2> /dev/null ; do echo -n "." ; sleep 3 ; done
 
 # Create and run mock VOTO API container
 docker run \
   -d \
   -p "0:8089" \
-  --name ulizatests_voto \
+  --name ulizatestsvoto \
   farmradio/voto_mock_api
 
 # Create and run Uliza API container
@@ -38,11 +38,11 @@ docker run \
   -e "DB_NAME=uliza_core" \
   -e "DB_USER=root" \
   -e "DB_PASSWORD=root" \
-  -e "DB_SERVICE_HOST=ulizatests_db" \
+  -e "DB_SERVICE_HOST=ulizatestsdb" \
   -e "DB_SERVICE_PORT=3306" \
   -p "0:8000" \
-  --link ulizatests_db \
-  --name ulizatests_api \
+  --link ulizatestsdb \
+  --name ulizatestsapi \
   farmradio/uliza_api
 
 # Create and run registration service container
@@ -51,18 +51,18 @@ docker run \
   -p "0:3034" \
   -e "PORT=3034" \
   -e "LOG_LEVEL=DEBUG" \
-  -e "VOTO_API_URL=http://ulizatests_voto:8089/api/v1" \
-  -e "ULIZA_API_URL=http://ulizatests_api:8000/api/v1" \
+  -e "VOTO_API_URL=http://ulizatestsvoto:8089/api/v1" \
+  -e "ULIZA_API_URL=http://ulizatestsapi:8000/api/v1" \
   -e "CALL_SCHEDULE_OFFSET=600" \
   -e "MIN_RESCHEDULE_DELAY=172800" \
-  --link ulizatests_api \
-  --link ulizatests_voto \
-  --name ulizatests_middleware \
+  --link ulizatestsapi \
+  --link ulizatestsvoto \
+  --name ulizatestsmiddleware \
   farmradio/registration_service
 
 # Apply database migrations
-docker exec -it ulizatests_api ./django.sh makemigrations
-docker exec -it ulizatests_api ./django.sh migrate
+docker exec -it ulizatestsapi ./django.sh makemigrations 
+docker exec -it ulizatestsapi ./django.sh migrate
 
 function get_port () {
   docker ps \
@@ -72,11 +72,11 @@ function get_port () {
 
 # Collect ENV variables for tests
 OUT="\
-REG_SERVICE_URL=http://0.0.0.0:$(get_port ulizatests_middleware)\n\
+REG_SERVICE_URL=http://0.0.0.0:$(get_port ulizatestsmiddleware)\n\
 DB_HOST=0.0.0.0\n\
-DB_PORT=$(get_port ulizatests_db)\n\
-ULIZA_API_URL=http://0.0.0.0:$(get_port ulizatests_api)/api/v1\n\
-VOTO_API_URL=http://0.0.0.0:$(get_port ulizatests_voto)/api/v1\n\
+DB_PORT=$(get_port ulizatestsdb)\n\
+ULIZA_API_URL=http://0.0.0.0:$(get_port ulizatestsapi)/api/v1\n\
+VOTO_API_URL=http://0.0.0.0:$(get_port ulizatestsvoto)/api/v1\n\
 VOTO_API_KEY=XXX_TEST_KEY_XXX"
 
 echo -e "---------------------------------------------------\
@@ -95,7 +95,7 @@ docker ps
 
 # API
 URL=$(docker ps \
-  --filter "name=ulizatests_api" \
+  --filter "name=ulizatestsapi" \
   --format "{{.Ports}}" | sed -n 's/\([0-9.:]*\).*/\1/p')
 echo -e "---------------------------------------------------\
   \nUliza API url: http://$URL/api/v1"
