c873629b0089552fe963a73a6500fabfb1fbe233
Johannes Hilden <hildenjohannes@gmail.com>
2017-11-28 20:01:45 +0300

    Enforce trailing slash on all urls

--- django-api/uliza/urls.py
+++ django-api/uliza/urls.py
@@ -14,24 +14,24 @@ from uliza.views.VotoSurveyRegistrationTree import (
 from uliza.views.APIRoot import APIRoot
 
 participants = [
-    url(r'^(?P<id>\d+)$', ParticipantsInstance.as_view()),
-    url(r'$', Participants.as_view()),
+    url(r'^(?P<id>\d+)/$', ParticipantsInstance.as_view()),
+    url(r'^$', Participants.as_view()),
 ]
 
 registration_calls = [
-    url(r'^(?P<id>\d+)$', RegistrationCallsInstance.as_view()),
-    url(r'$', RegistrationCalls.as_view()),
+    url(r'^(?P<id>\d+)/$', RegistrationCallsInstance.as_view()),
+    url(r'^$', RegistrationCalls.as_view()),
 ]
 
 voto_webhook_log = [
-    url(r'^(?P<id>\d+)$', VotoWebhookLogInstance.as_view()),
-    url(r'$', VotoWebhookLogCollection.as_view()),
+    url(r'^(?P<id>\d+)/$', VotoWebhookLogInstance.as_view()),
+    url(r'^$', VotoWebhookLogCollection.as_view()),
 ]
 
 voto_survey_registration_tree = [
-    url(r'^(?P<voto_survey_id>\d+)$',
+    url(r'^(?P<voto_survey_id>\d+)/$',
         VotoSurveyRegistrationTreeInstance.as_view()),
-    url(r'$', VotoSurveyRegistrationTreeCollection.as_view()),
+    url(r'^$', VotoSurveyRegistrationTreeCollection.as_view()),
 ]
 
 urlpatterns = [
