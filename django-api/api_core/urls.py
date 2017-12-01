75ff3ed2f1f1f7bbac66c69f121dd9fb6f1db56a
Johannes Hilden <hildenjohannes@gmail.com>
2017-12-01 14:38:19 +0300

    Make commit before Jigsa

--- django-api/api_core/urls.py
+++ django-api/api_core/urls.py
@@ -2,9 +2,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from rest_framework.documentation import include_docs_urls
 
+schema_patters = [url(r'^$', include('uliza.urls'))]
+
 urlpatterns = [
     url(r'^admin', include(admin.site.urls)),
     url(r'^api/v1/', include('log_app.urls')),
     url(r'^api/v1/', include('uliza.urls')),
-    url(r'^api/docs/', include_docs_urls(title='Uliza API')),
+    url(r'^api/docs/', include_docs_urls(title='Uliza API',
+        patterns=schema_patters)),
 ]
