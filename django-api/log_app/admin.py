6ac538a8dc73e4a6333e3597ef4ef29581f84543
Jigsa Tesfaye <jiks.tes@gmail.com>
2017-12-11 09:36:02 +0300

    Show program model in admin

--- django-api/log_app/admin.py
+++ django-api/log_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from log_app.models import Contact,Auth0User
+from log_app.models import Contact,Auth0User,Program
 
 admin.site.register(Auth0User)
 admin.site.register(Contact)
+admin.site.register(Program)
