b199e7230986ce20bb7e4cc88f24536c531b613c
Johannes Hilden <hildenjohannes@gmail.com>
2017-12-11 12:35:36 +0300

    Include log_app API endpoints in docs again

--- django-api/api_core/urls.py
+++ django-api/api_core/urls.py
@@ -2,12 +2,9 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from rest_framework.documentation import include_docs_urls
 
-schema_patterns = [url(r'^api/v1/', include('uliza.urls'))]
-
 urlpatterns = [
     url(r'^admin', include(admin.site.urls)),
     url(r'^api/v1/', include('log_app.urls')),
     url(r'^api/v1/', include('uliza.urls')),
-    url(r'^api/docs/', include_docs_urls(title='Uliza API',
-        patterns=schema_patterns)),
+    url(r'^api/docs/', include_docs_urls(title='Uliza API')),
 ]
