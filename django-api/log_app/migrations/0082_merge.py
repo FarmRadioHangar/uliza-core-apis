a6f2397ff83ced926207a2e65f77610524d06740
Johannes Hilden <hildenjohannes@gmail.com>
2017-12-19 11:35:33 +0300

    Add migration

---
+++ django-api/log_app/migrations/0082_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('log_app', '0079_auto_20171122_0907'),
+        ('log_app', '0081_auto_20171127_1201'),
+    ]
+
+    operations = [
+    ]
