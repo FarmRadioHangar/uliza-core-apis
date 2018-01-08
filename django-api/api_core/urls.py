7ff1ef319f1a379c89f7c51189ccc1f9d0e65b64
Johannes Hilden <hildenjohannes@gmail.com>
2018-01-08 17:44:39 +0300

    Add api-token-auth url pattern

--- django-api/api_core/urls.py
+++ django-api/api_core/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from rest_framework.documentation import include_docs_urls
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     url(r'^admin', include(admin.site.urls)),
     url(r'^api/v1/', include('log_app.urls')),
     url(r'^api/v1/', include('uliza.urls')),
     url(r'^api/docs/', include_docs_urls(title='Uliza API')),
+    url(r'^api-token-auth/', obtain_jwt_token),
 ]
