d757b547a6831a4c76c4dcb39791b490a4cb6fee
Johannes Hilden <hildenjohannes@gmail.com>
2018-01-09 13:34:47 +0300

    Add api-token-verify/ url pattern

--- django-api/api_core/urls.py
+++ django-api/api_core/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from rest_framework.documentation import include_docs_urls
 from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import verify_jwt_token
 
 urlpatterns = [
     url(r'^admin', include(admin.site.urls)),
@@ -9,4 +10,5 @@ urlpatterns = [
     url(r'^api/v1/', include('uliza.urls')),
     url(r'^api/docs/', include_docs_urls(title='Uliza API')),
     url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-verify/', verify_jwt_token),
 ]
