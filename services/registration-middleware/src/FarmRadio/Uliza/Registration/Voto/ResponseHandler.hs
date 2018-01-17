62fc9863fd9b3d4d0d5179af9d8082e0b1aad3b9
Johannes Hilden <hildenjohannes@gmail.com>
2018-01-17 17:00:26 +0300

    Update response handler to accept tree id in request params.

--- services/registration-middleware/src/FarmRadio/Uliza/Registration/Voto/ResponseHandler.hs
+++ services/registration-middleware/src/FarmRadio/Uliza/Registration/Voto/ResponseHandler.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
 module FarmRadio.Uliza.Registration.Voto.ResponseHandler
   ( votoResponse
   ) where
@@ -10,14 +9,26 @@ import Data.Aeson
 import Data.Either.Utils                              ( maybeToEither )
 import Data.Monoid
 import Data.Text
+import Data.URLEncoded                                ( URLEncoded )
 import FarmRadio.Uliza.Api.Participant
 import FarmRadio.Uliza.Api.RegistrationCall
 import FarmRadio.Uliza.Api.Utils
 import FarmRadio.Uliza.Registration
 import FarmRadio.Uliza.Registration.Logger
 import FarmRadio.Uliza.Registration.SurveyTreePair
+import Text.Read                                      ( readMaybe )
 
+import qualified Data.URLEncoded                      as URLEncoded
 import qualified Data.ByteString.Lazy.Char8           as B8
+
+getTreeId :: Int -> RegistrationHandler (Maybe Int)
+getTreeId survey = do
+    state <- get
+    case URLEncoded.lookup ("tree" :: String) (state ^. params) of
+      Just t -> return (readMaybe t)
+      Nothing -> do
+        tree <- getSurveyTreeAssociation survey
+        return (votoTreeId <$> tree)
 
 -- | Response handler for survey response webhook -- triggered by VOTO every
 --   time a survey response is recorded.
@@ -31,7 +42,7 @@ votoResponse = do
       [ ("data"     , String (toText body))
       , ("endpoint" , "responses") ]
     phone  <- extract "subscriber_phone"
-    survey <- extract "survey_id" 
+    survey <- extract "survey_id"
     -- If the phone number is not already associated with a participant in the
     -- database, one is created here
     participant <- getOrCreateParticipant phone
@@ -39,23 +50,24 @@ votoResponse = do
     call <- getRegistrationCall participant
     -- Determine the participant's registration status
     status <- determineRegistrationStatus participant call
-    -- Get the registration tree associated with this survey (if one exists)
-    tree <- getSurveyTreeAssociation survey
+    -- Find the tree id for this survey, from the request params or (if absent)
+    -- from Uliza
+    treeId <- getTreeId survey
     -- Take some action, then log and respond
-    case (tree, status) of
+    case (treeId, status) of
       ( _, AlreadyRegistered )    -> noop "ALREADY_REGISTERED"
       ( _, RegistrationDeclined ) -> noop "REGISTRATION_DECLINED"
       ( _, PriorCallScheduled )   -> noop "PRIOR_CALL_SCHEDULED"
       ( _, RecentCallMade )       -> noop "TOO_SOON"
       ( Nothing, _ )              -> noop "NO_REGISTRATION_TREE"
-      ( Just SurveyTreePair{..}, ScheduleCall time ) -> do
-        logDebugJSON "survery_registration_tree" SurveyTreePair{..} & liftIO
-        call <- scheduleRegistrationCall participant time votoTreeId
+      ( Just tree, ScheduleCall time ) -> do
+        logDebug "survery_registration_tree" (show tree) & liftIO
+        call <- scheduleRegistrationCall participant time tree
         return $ toJSON $ object
           [ ("action", "REGISTRATION_CALL_SCHEDULED")
           , ("registration_call", toJSON call) ]
   where
-    -- Request successfully processed, but a registration call was not scheduled
+    -- Request successfully processed, but no registration call was scheduled
     noop :: Text -> RegistrationHandler Value
     noop reason = do
       liftIO $ logNotice "no_call_scheduled"
