de2015db15e2820e033779ab6c50f0746c3e96a8
Johannes Hilden <hildenjohannes@gmail.com>
2018-01-24 12:01:15 +0300

    Add voto_call_id and tree_id to Reg. call object

--- services/registration-middleware/src/FarmRadio/Uliza/Registration/RegistrationCall.hs
+++ services/registration-middleware/src/FarmRadio/Uliza/Registration/RegistrationCall.hs
@@ -9,6 +9,8 @@ data RegistrationCall = RegistrationCall
     { entityId           :: !(Maybe Int)
     , phoneNumber        :: !Text
     , scheduledTime      :: !Text 
+    , votoCallId         :: !Int
+    , votoTreeId         :: !Int
     , createdAt          :: !Text }
   deriving (Show, Eq)
 
@@ -17,6 +19,8 @@ instance ToJSON RegistrationCall where
     [ "id"             .= entityId
     , "phone_number"   .= phoneNumber
     , "scheduled_time" .= scheduledTime
+    , "voto_call_id"   .= votoCallId
+    , "voto_tree_id"   .= votoTreeId
     , "created_at"     .= createdAt ]
 
 instance FromJSON RegistrationCall where
@@ -24,4 +28,6 @@ instance FromJSON RegistrationCall where
     <$> v .:? "id"
     <*> v .:  "phone_number"
     <*> v .:  "scheduled_time"
+    <*> v .:  "voto_call_id"
+    <*> v .:  "voto_tree_id"
     <*> v .:  "created_at"
