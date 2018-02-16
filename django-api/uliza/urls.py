380836b9da0c903fe2156001d89b34b5959113d7
Johannes Hilden <hildenjohannes@gmail.com>
2018-02-16 11:31:54 +0300

    Update url structure

--- django-api/uliza/urls.py
+++ django-api/uliza/urls.py
@@ -4,44 +4,51 @@ from uliza.views.Participants import (
         ParticipantsInstance)
 from uliza.views.RegistrationCalls import (
         RegistrationCalls,
-        RegistrationCallsInstance)
-from uliza.views.VotoWebhookLog import (
-        VotoWebhookLogCollection,
-        VotoWebhookLogInstance)
-from uliza.views.VotoSurveyRegistrationTree import (
-        VotoSurveyRegistrationTreeCollection,
-        VotoSurveyRegistrationTreeInstance)
+        RegistrationCallsInstance,
+        RegistrationCallsVotoInstance,
+        RegistrationCallsParticipant)
+# from uliza.views.VotoWebhookLog import (
+#         VotoWebhookLogCollection,
+#         VotoWebhookLogInstance)
+# from uliza.views.VotoSurveyRegistrationTree import (
+#         VotoSurveyRegistrationTreeCollection,
+#         VotoSurveyRegistrationTreeInstance)
 from uliza.views.APIRoot import APIRoot
 
 participants = [
+    url(r'^(?P<participant>\d+)/registration_call/$',
+        RegistrationCallsParticipant.as_view()),
     url(r'^(?P<id>\d+)/$', ParticipantsInstance.as_view()),
     url(r'^$', Participants.as_view()),
 ]
 
 registration_calls = [
     url(r'^(?P<id>\d+)/$', RegistrationCallsInstance.as_view()),
+    url(r'^voto_call/(?P<voto_call_id>\d+)/$',
+        RegistrationCallsVotoInstance.as_view()),
     url(r'^$', RegistrationCalls.as_view()),
 ]
 
-voto_webhook_log = [
-    url(r'^(?P<id>\d+)/$', VotoWebhookLogInstance.as_view()),
-    url(r'^$', VotoWebhookLogCollection.as_view()),
-]
+# voto_webhook_log = [
+#     url(r'^(?P<id>\d+)/$', VotoWebhookLogInstance.as_view()),
+#     url(r'^$', VotoWebhookLogCollection.as_view()),
+# ]
 
-voto_survey_registration_tree = [
-    url(r'^(?P<voto_survey_id>\d+)/$',
-        VotoSurveyRegistrationTreeInstance.as_view()),
-    url(r'^$', VotoSurveyRegistrationTreeCollection.as_view()),
-]
+# voto_survey_registration_tree = [
+#     url(r'^(?P<voto_survey_id>\d+)/$',
+#         VotoSurveyRegistrationTreeInstance.as_view()),
+#     url(r'^$', VotoSurveyRegistrationTreeCollection.as_view()),
+# ]
 
 urlpatterns = [
     url(r'^$', APIRoot.as_view()),
     url(r'^participants/', include(participants, 'participants')),
     url(r'^registration_calls/', include(registration_calls,
         'registration_calls')),
-    url(r'^voto_webhook_log/', include(voto_webhook_log, 'voto_webhook_log')),
-    url(r'^voto_survey_registration_tree/', include(
-        voto_survey_registration_tree,
-        'voto_survey_registration_tree'
-    ))
+
+    # url(r'^voto_webhook_log/', include(voto_webhook_log, 'voto_webhook_log')),
+    # url(r'^voto_survey_registration_tree/', include(
+    #     voto_survey_registration_tree,
+    #     'voto_survey_registration_tree'
+    # ))
 ]
