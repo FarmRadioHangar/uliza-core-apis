683fbdbd663c970b3b0046099dfd82b1d2cc057d
Johannes Hilden <hildenjohannes@gmail.com>
2018-02-22 21:15:13 +0300

    Remove commented out code

--- django-api/uliza/urls.py
+++ django-api/uliza/urls.py
@@ -4,15 +4,10 @@ from uliza.views.Participants import (
         ParticipantsInstance)
 from uliza.views.RegistrationCalls import (
         RegistrationCalls,
+        RegistrationCallsMissingLocation,
         RegistrationCallsInstance,
         RegistrationCallsVotoInstance,
         RegistrationCallsParticipant)
-# from uliza.views.VotoWebhookLog import (
-#         VotoWebhookLogCollection,
-#         VotoWebhookLogInstance)
-# from uliza.views.VotoSurveyRegistrationTree import (
-#         VotoSurveyRegistrationTreeCollection,
-#         VotoSurveyRegistrationTreeInstance)
 from uliza.views.APIRoot import APIRoot
 
 participants = [
@@ -24,31 +19,15 @@ participants = [
 
 registration_calls = [
     url(r'^(?P<id>\d+)/$', RegistrationCallsInstance.as_view()),
+    url(r'^missing_location/$', RegistrationCallsMissingLocation.as_view()),
     url(r'^voto_call/(?P<voto_call_id>\d+)/$',
         RegistrationCallsVotoInstance.as_view()),
     url(r'^$', RegistrationCalls.as_view()),
 ]
 
-# voto_webhook_log = [
-#     url(r'^(?P<id>\d+)/$', VotoWebhookLogInstance.as_view()),
-#     url(r'^$', VotoWebhookLogCollection.as_view()),
-# ]
-
-# voto_survey_registration_tree = [
-#     url(r'^(?P<voto_survey_id>\d+)/$',
-#         VotoSurveyRegistrationTreeInstance.as_view()),
-#     url(r'^$', VotoSurveyRegistrationTreeCollection.as_view()),
-# ]
-
 urlpatterns = [
     url(r'^$', APIRoot.as_view()),
     url(r'^participants/', include(participants, 'participants')),
     url(r'^registration_calls/', include(registration_calls,
-        'registration_calls')),
-
-    # url(r'^voto_webhook_log/', include(voto_webhook_log, 'voto_webhook_log')),
-    # url(r'^voto_survey_registration_tree/', include(
-    #     voto_survey_registration_tree,
-    #     'voto_survey_registration_tree'
-    # ))
+        'registration_calls'))
 ]
