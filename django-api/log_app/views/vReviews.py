6976d0465792d73b6f671e2680933db5de09000b
Jigsa Tesfaye <jiks.tes@gmail.com>
2018-04-30 16:30:29 +0300

    add filter by program for reviews

--- django-api/log_app/views/vReviews.py
+++ django-api/log_app/views/vReviews.py
@@ -3,7 +3,7 @@ from rest_framework import generics
 from rest_framework import status
 from rest_framework.exceptions import NotFound
 from rest_framework.views import APIView
-from log_app.models import Review
+from log_app.models import Review,Log
 from log_app.serializers import ReviewSerializer
 import django_filters
 from rest_framework import filters
@@ -13,6 +13,17 @@ class ReviewGet(generics.ListCreateAPIView):
     model = Review
     serializer_class = ReviewSerializer
     filter_fields = ['id', 'log', 'reviewer']
+
+    def get_queryset(self):
+        program = self.request.GET.get('program')
+        if program:
+            logs = Log.objects.filter(program=program).values('id')
+            queryset = Review.objects.filter(log__in=logs)
+        else:
+            queryset = Review.objects.all()
+
+        return queryset
+
 
 class ReviewEntity(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
