a4a6d902dafb252672617c00781d57ccad4f583f
Jigsa Tesfaye <jiks.tes@gmail.com>
2018-08-27 17:08:31 +0300

    add endpoint to handle user migration for auth0 custom db

--- django-api/log_app/views/vAuth0User.py
+++ django-api/log_app/views/vAuth0User.py
@@ -3,8 +3,11 @@ from rest_framework import generics
 from rest_framework import status
 from rest_framework.exceptions import NotFound
 from rest_framework.views import APIView
-from log_app.models import Auth0User
+from log_app.models import Auth0User,Contact
 from log_app.serializers import Auth0UserSerializer
+from django.http import Http404,HttpResponse
+from django.contrib.auth.hashers import check_password
+from django.views.decorators.http import require_POST
 
 import django_filters
 from rest_framework import filters
@@ -21,3 +24,27 @@ class Auth0UserEntity(generics.RetrieveUpdateAPIView):
     model = Auth0User
     serializer_class = Auth0UserSerializer
     lookup_field = 'id'
+
+
+@require_POST
+def authenticate(request):
+    try:
+        user = Auth0User.objects.get(username=request.POST['username'])
+    except Auth0User.DoesNotExist:
+        return Http404
+
+    # Check password
+    if check_password(request.POST['password'],user.password):
+        try:
+            contact = Contact.objects.get(user_id=user.id)
+        except Contact.DoesNotExist:
+            raise Http404('User does not exist')
+
+        user = {'username':user.username,'name':contact.first_name+' '+contact.last_name,'email':user.email}
+
+        import json
+        user = json.dumps(user)
+
+        return HttpResponse(user,content_type = 'application/javascript; charset=utf8')
+    else:
+        raise Http404('User does not exist')
