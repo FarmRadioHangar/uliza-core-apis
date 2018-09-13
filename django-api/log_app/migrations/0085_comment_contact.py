a945db9c6945a846aabf084ebebc79e0147fe584
Jigsa Tesfaye <jiks.tes@gmail.com>
2018-09-13 06:22:06 +0300

    change comment user to contact model

--- django-api/log_app/migrations/0085_comment_contact.py
+++ django-api/log_app/migrations/0085_comment_contact.py
@@ -3,6 +3,20 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 
+def change_comment_user_to_contact(apps,schema_editor):
+    Contact = apps.get_model('log_app','Contact')
+    Auth0User = apps.get_model('log_app','Auth0User')
+    Comment = apps.get_model('log_app','Comment')
+
+    comments = Comment.objects.all()
+
+    for comment in comments:
+        user = Auth0User.objects.filter(username = comment.user.username)
+
+        if user:
+            contact = Contact.objects.filter(user_id='auth0|'+str(user[0].id))
+            comment.contact = contact[0]
+            comment.save()
 
 class Migration(migrations.Migration):
 
@@ -16,4 +30,5 @@ class Migration(migrations.Migration):
             name='contact',
             field=models.ForeignKey(to='log_app.Contact', null=True),
         ),
+        migrations.RunPython(change_comment_user_to_contact)
     ]
