a4e5bfc3301c89549da42d251d068884b84d9710
Jigsa Tesfaye <jiks.tes@gmail.com>
2019-01-23 15:56:06 +0300

    add field gdrive_url

--- django-api/log_app/models.py
+++ django-api/log_app/models.py
@@ -271,6 +271,7 @@ class Log(models.Model):
 
     gdrive = models.FileField(upload_to='/FRI-LOG',storage=GDRIVE_STORAGE, null=True,blank=True)
     gdrive_available = models.BooleanField(default=False)
+    gdrive_url = models.URLField(max_length=400,null=True,blank=True)
     recording_backup = models.FileField(null=True,blank=True)
     recording_saved = models.BooleanField(default=True)
     offset = models.PositiveIntegerField(default=0)
