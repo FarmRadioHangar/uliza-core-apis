b38d874a23e8c246983b955973358b963a959699
Jigsa Tesfaye <jiks.tes@gmail.com>
2019-03-05 13:42:51 +0300

    pagination for radiostation projects endpoint

--- django-api/log_app/views/vRadiostations.py
+++ django-api/log_app/views/vRadiostations.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.decorators import api_view
 from rest_framework import generics
 from rest_framework import status
@@ -23,9 +24,18 @@ class RadioStationEntity(generics.RetrieveUpdateAPIView):
     lookup_field = 'id'
 
 @api_view(['GET'])
-def radion_station_projects(request,id):
+def radio_station_projects(request,id):
+    context = {'request', request}
+    paginator = PageNumberPagination()
+
+    if 'page_size' in request.GET:
+        paginator.page_size = request.GET['page_size']
+    else:
+        paginator.page_size = 4
+
     programs = Program.objects.filter(radio_station__id=id).values_list('project__id',flat=True)
     projects = Project.objects.filter(pk__in=programs)
+    projects = paginator.paginate_queryset(projects,request)
     projects = ProjectSerializer(projects,many=True)
 
-    return Response(projects.data)
+    return paginator.get_paginated_response(projects.data)
