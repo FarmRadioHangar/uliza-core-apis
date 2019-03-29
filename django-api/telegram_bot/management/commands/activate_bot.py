e22a15007d205ef0f2ac59ac6f60b428089de243
Jigsa Tesfaye <jiks.tes@gmail.com>
2019-03-29 12:16:57 +0300

    activate telegram bot command

--- django-api/telegram_bot/management/commands/activate_bot.py
+++ django-api/telegram_bot/management/commands/activate_bot.py
@@ -1,6 +1,12 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import translation
+from api_core.settings import BASE_DIR,TELEGRAM_TOKEN
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        print "activating bot"
+        url = "https://api.telegram.org/bot"+TELEGRAM_TOKEN+"/setWebhook"
+
+        response = requests.post("https://dev.uliza.fm/api/v1/telegram/",
+                      file('certificate',BASE_DIR+"/fullchain.pem"))
+                      
+        print response.content
