f03ba454f03acf38ed0e341b5196a873f3b5866f
Jigsa Tesfaye <jiks.tes@gmail.com>
2019-03-31 15:14:24 +0300

    update the bot activation code for further testing

--- django-api/telegram_bot/management/commands/activate_bot.py
+++ django-api/telegram_bot/management/commands/activate_bot.py
@@ -1,5 +1,4 @@
 from django.core.management.base import BaseCommand, CommandError
-from django.utils import translation
 from api_core.settings import BASE_DIR,TELEGRAM_TOKEN
 import requests
 
@@ -7,7 +6,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         url = "https://api.telegram.org/bot"+TELEGRAM_TOKEN+"/setWebhook"
 
-        response = requests.post("https://dev.uliza.fm/api/v1/telegram/",
+        response = requests.post(url,{"url":"https://dev.uliza.fm/api/v1/telegram"},
                       files=dict(certificate=BASE_DIR+"/fullchain.pem"))
-                      
-        print response.content
+
+        import pdb; pdb.set_trace()
+        print response
--- django-api/telegram_bot/views.py
+++ django-api/telegram_bot/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseForbidden
 
+
+from api_core.settings import BASE_DIR,TELEGRAM_TOKEN
+import requests
+
+def activate(request):
+    url = "https://api.telegram.org/bot"+TELEGRAM_TOKEN+"/setWebhook"
+
+    response = requests.post(url,{"url":"https://dev.uliza.fm/api/v1/telegram"},
+                  files=dict(certificate=BASE_DIR+"/fullchain.pem"))
+
+    return HttpResponse(response.content)
+
 def start(request):
     return HttpResponse('This is FRI speaking... what can I help you?')
