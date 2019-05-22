36e6810a9d72e81b6291443abd15c69fb281ba38
Jigsa Tesfaye <jiks.tes@gmail.com>
2019-05-22 15:47:25 +0300

    allow reentry for comment convo handler

--- django-api/telegram_bot/telegrambot.py
+++ django-api/telegram_bot/telegrambot.py
@@ -163,9 +163,9 @@ def main():
     #comments
     comment_handler = ConversationHandler(
         entry_points=[CallbackQueryHandler(comment_instruction,pattern='/add_comment*')],
-        states = {0: [MessageHandler(Filters.text,add_comment)]},
-        fallbacks = [CommandHandler('cancel', start)],
-        allow_reentry=True
+        states = {0: [CallbackQueryHandler(comment_instruction,pattern='/add_comment*')],
+                  1: [MessageHandler(Filters.text,add_comment)]},
+        fallbacks = [CommandHandler('cancel', start)]
     )
     dp.add_handler(comment_handler)
     dp.add_handler(CallbackQueryHandler(show_comments,pattern='/show_comments*'))
