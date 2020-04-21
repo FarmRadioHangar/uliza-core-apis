2073d5203c99364c8965348779c19f96ea7b5095
Jigsa Tesfaye <jiks.tes@gmail.com>
2020-04-21 09:47:26 +0300

    open covid data admin access

--- django-api/covid/admin.py
+++ django-api/covid/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from covid.models import Content
+from covid.models import *
 # Register your models here.
 
 class ContentAdmin(admin.ModelAdmin):
     pass
 
+class ChatUserAdmin(admin.ModelAdmin):
+    pass
+
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(ChatUser, ChatUserAdmin)
 admin.site.register(Content, ContentAdmin)
