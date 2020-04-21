26051cf556cf368d6dac8e30379fc6458e947513
Jigsa Tesfaye <jiks.tes@gmail.com>
2020-04-21 09:48:58 +0300

    setup covid api endpooints

--- django-api/covid/urls.py
+++ django-api/covid/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import patterns, include, url
 from api_core.settings import TELEGRAM_TOKEN
+from covid.views import *
 
 urlpatterns = patterns('',
-    url('(?P<lang>\w+)/(?P<topic>\w+)$','covid.views.content',name='covid_content'),
+    url('covid19/chat_users$',ChatUserGet.as_view()),
+    url('covid19/chat_users/(?P<id>\d+)$',ChatUserEntity.as_view()),
+    url('covid19/questions$',QuestionGet.as_view()),
+    url('covid19/questions/(?P<id>\d+)$',QuestionEntity.as_view()),
+    url('covid19/topic/(?P<lang>\w+)/(?P<topic>\w+)$','covid.views.content',name='covid_content'),
 )
--- django-api/covid/views.py
+++ django-api/covid/views.py
@@ -1,17 +1,63 @@
 from django.shortcuts import render
-from covid.models import Content
+from covid.models import *
+from covid.serializers import *
 from django.http import HttpResponse
 
-# Create your views here.
+from rest_framework import generics
+
+
+class ContentGet(generics.ListCreateAPIView):
+    queryset = Content.objects.all()
+    model = Content
+    serializer_class = ContentSerializer
+    filter_fields = ['id','topic_en']
+
+class ContentEntity(generics.RetrieveUpdateAPIView):
+
+    queryset = Content.objects.all()
+    model = Content
+    serializer_class = ContentSerializer
+    lookup_field = 'id'
+
+
+class ChatUserGet(generics.ListCreateAPIView):
+    queryset = ChatUser.objects.all()
+    model = ChatUser
+    serializer_class = ChatUserSerializer
+    filter_fields = ['id','user_id']
+
+
+class ChatUserEntity(generics.RetrieveUpdateAPIView):
+    queryset = ChatUser.objects.all()
+    model = ChatUser
+    serializer_class = ChatUserSerializer
+    lookup_field = 'id'
+
+class QuestionGet(generics.ListCreateAPIView):
+    queryset = Question.objects.all()
+    model = Question
+    serializer_class = QuestionSerializer
+    filter_fields = ['id','content']
+
+    def perform_create(self, serializer):
+        chat_user = ChatUser.objects.get(user_id=self.request._data['user_id'])
+        chat_user.full_name = self.request._data['full_name']
+        chat_user.radio_station = self.request._data['radio_station']
+        chat_user.save()
+        serializer.save(chat_user = chat_user)
+
+
+
+
+class QuestionEntity(generics.RetrieveUpdateAPIView):
+    queryset = Question.objects.all()
+    model = Question
+    serializer_class = QuestionSerializer
+    lookup_field = 'id'
+
+
 def content(request,topic,lang):
-    if lang == 'en':
-        content = Content.objects.filter(topic_en=topic)
-    elif lang == 'fr':
-        content = Content.objects.filter(topic_fr=topic)
-    elif lang == 'am':
-        content = Content.objects.filter(topic_am=topic)
-    else:
-        HttpResponse('Content error')
+    content = Content.objects.filter(topic_en=topic)
 
     if len(content)==0:
         return HttpResponse('Content error')
