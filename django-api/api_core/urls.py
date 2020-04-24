9e36983d3e4055f99e2bd05959d03b1d6c731d8f
Jigsa Tesfaye <jiks.tes@gmail.com>
2020-04-24 17:22:24 +0300

    small refactoring

--- django-api/api_core/urls.py
+++ django-api/api_core/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from rest_framework.documentation import include_docs_urls
-from settings import TELEGRAM_TOKEN
 
 urlpatterns = [
     url(r'^api/admin', include(admin.site.urls)),
