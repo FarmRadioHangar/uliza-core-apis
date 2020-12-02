fbb98b34c28207109b1d2f7c75bcf763038af4de
JiXa <jiks.tes@gmail.com>
2020-12-02 14:49:21 +0300

    add endpoint to update project related formats

--- django-api/log_app/views/vFormats.py
+++ django-api/log_app/views/vFormats.py
@@ -7,12 +7,14 @@ from log_app.models import Format
 from log_app.serializers import FormatSerializer
 import django_filters
 from rest_framework import filters
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 class FormatGet(generics.ListCreateAPIView):
     queryset = Format.objects.all()
     model = Format
     serializer_class = FormatSerializer
-    filter_fields = ['id','name','legacy','always_checked']
+    filter_fields = ['id','name','legacy','always_checked','project_related','projects']
 
     def get_queryset(self):
         pk_list = self.request.GET.get('pk_list')
@@ -29,3 +31,21 @@ class FormatEntity(generics.RetrieveUpdateAPIView):
     model = Format
     serializer_class = FormatSerializer
     lookup_field = 'id'
+
+@api_view(['PATCH'])
+def update_access(request,id):
+    """
+    Update the list of projects the format is used in
+    """
+    from log_app.models import Project,Format
+
+    format = Format.objects.get(pk=id)
+    access = request.data['access'].split(',')
+    projects = Project.objects.filter(id__in=access)
+
+    for p in projects:
+        format.projects.add(p)
+
+    format.save()
+
+    return Response(access, status=status.HTTP_200_OK)
