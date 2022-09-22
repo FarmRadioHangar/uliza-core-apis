61df377ed0f4d5a4c8e3f5d4769cd8dc8da64060
JiXa <jiks.tes@gmail.com>
2022-09-22 13:33:39 +0300

    query to exclude pt indicators

--- django-api/log_app/views/vIndicators.py
+++ django-api/log_app/views/vIndicators.py
@@ -18,6 +18,17 @@ class IndicatorGet(generics.ListCreateAPIView):
     ordering_fields = ('order','grouping')
     filter_fields = ['id','grouping','project_tied']
 
+    def get_queryset(self):
+        pt = self.request.GET.get('pt')
+        if pt == '0':
+            queryset = Indicator.objects.filter(project_tied = None)
+        else:
+            queryset = Indicator.objects.all()
+
+        return queryset
+
+
+
 class IndicatorEntity(generics.RetrieveUpdateDestroyAPIView):
     queryset = Indicator.objects.all()
     model = Indicator
