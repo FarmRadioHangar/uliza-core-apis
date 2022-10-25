882689de7832a741e390c026dd1ad72df9c464c1
Jigsa <jiks.tes@gmail.com>
2022-10-25 11:33:44 +0300

    - filtering features for all country/all projects - exporting is now available

--- django-api/log_app/views/vTargets.py
+++ django-api/log_app/views/vTargets.py
@@ -64,21 +64,38 @@ def target_stats(request):
     from django.utils.dateparse import parse_date,parse_datetime
     from django.db.models import Sum,Avg
 
-    project = Project.objects.filter(id=request.GET['project_id'])
-    if request.GET['multi_country'] == 'true':
-        project = Project.objects.filter(code=project[0].code)
+    # Project filter
+    if not request.GET['project_id'] == '0':
+        project = Project.objects.filter(id=request.GET['project_id'])
+
+        if request.GET['country'] == 'multi_country':
+            project = Project.objects.filter(code=project[0].code,country__exclude=False)
+    else:
+        # Country filter
+        if request.GET['country'] == 'all':
+            project = Project.objects.filter(country__exclude=False)
+        elif not request.GET['country'] == 'multi_country':
+            project = Project.objects.filter(country__id = request.GET['country'],country__exclude=False)
+        else:
+            project = Project.objects.filter(country__exclude=False)
+
 
     if 'start_date' in request.GET and 'end_date' in request.GET:
         start_datetime = parse_datetime(request.GET['start_date']+' 00:00')
         start_date = parse_date(request.GET['start_date'])
         end_date = parse_date(request.GET['end_date'])
-        programs = Program.objects.filter(project__in=project,end_date__gte = start_datetime,start_date__lte=end_date).exclude(project__country__exclude=True)
+        today = datetime.date.today()
+
+        if end_date > today:
+            end_date = today
+        programs = Program.objects.filter(project__in=project,end_date__gte = start_datetime,start_date__lte=end_date)
     else:
-        # I was born 08-04-1991
-        start_datetime = datetime.datetime(year=1991,month=4,day=8)
-        start_date = datetime.date(year=1991,month=4,day=8)
-        end_date = datetime.date(year=project[0].end_date.year+5,month=project[0].end_date.month,day=1)
-        programs = Program.objects.filter(project__in=project).exclude(project__country__exclude=True)
+        # Me was born 08-04-1991
+        start_datetime = datetime.datetime.now()
+        start_date = None
+        end_date = datetime.date.today()
+        # country and project filter to be done
+        programs = Program.objects.filter(project__in=project)
 
     total_hours = 0
     total_responses = 0
@@ -99,6 +116,11 @@ def target_stats(request):
 
     weekdays = ( 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
 
+    import csv
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="Uliza-Log-export'
+    writer = csv.writer(response)
+    writer.writerow(['Radio series code','Public name','Radio station name','Project name','Country','Episodes aired','Episodes reviewed','Airtime (mins)','start date','end_date'])
 
     for program in programs:
         avg_number_of_broadcast +=1
@@ -114,7 +136,7 @@ def target_stats(request):
         end_week_number = program.weeks
         program.start_date = program.start_date.replace(tzinfo=None)
 
-        if program.start_date < start_datetime:
+        if program.start_date <= start_datetime:
             week_diff = weeks_diff(program.start_date,start_datetime)
 
             start_week_number = week_diff[0]+1
@@ -122,7 +144,7 @@ def target_stats(request):
                 start_week_number +=1
 
 
-        if program.end_date > end_date:
+        if program.end_date >= end_date:
             week_diff = weeks_diff(end_date,program.end_date)
             end_week_number = week_diff[0]
             end_week_number = program.weeks-end_week_number
@@ -147,15 +169,6 @@ def target_stats(request):
         if number_of_episodes > 0:
             # todo there are times where there could be duplicate reviews for an episode
             reviews = Review.objects.filter(log__week__gte=start_week_number,log__week__lte=end_week_number,log__program=program).order_by('log__id')
-            if 'export' in request.GET:
-                try:
-                    if not program.public_name:
-                        public_name = ''
-                    else:
-                        public_name = program.public_name
-                        writer.writerow([program.name.encode('utf8'),public_name.encode('utf8'),program.radio_station.name.encode('utf8'),program.project.name.encode('utf8'),program.project.country.name.encode('utf8'),int(number_of_episodes),len(reviews),program.duration,str(program.start_date),str(program.end_date)])
-                except Exception as e:
-                    pass
 
             total_number_of_programs += 1
             start_week_number = int(start_week_number)
@@ -194,6 +207,17 @@ def target_stats(request):
 
             duration = duration*program_episodes_aired
             total_hours = total_hours + duration
+
+            if 'export' in request.GET:
+                try:
+                    if not program.public_name:
+                        public_name = ''
+                    else:
+                        public_name = program.public_name
+
+                    writer.writerow([program.name.encode('utf8'),public_name.encode('utf8'),program.radio_station.name.encode('utf8'),program.project.name.encode('utf8'),program.project.country.name.encode('utf8'),int(number_of_episodes),len(reviews),duration,str(program.start_date),str(program.end_date)])
+                except Exception as e:
+                    pass
 
             if not program.poll_program_id:
                 polls = PollSegment.objects.filter(program=program,episode_number__gte=start_week_number,episode_number__lte=end_week_number)
@@ -246,7 +270,10 @@ def target_stats(request):
     indicators = indicators.values()
     # targets = Target.objects.filter(project__id=project.id)
 
-    results = []
+    results = {}
+
+    if 'export' in request.GET:
+		return response
 
     from django.contrib.humanize.templatetags.humanize import naturalday
     for i in indicators:
@@ -266,13 +293,17 @@ def target_stats(request):
             i['target'] = t.aggregate(Sum('target_value'))['target_value__sum']
             i['total_result'] = t.aggregate(Sum('value'))['value__sum']
 
-            result = Report.objects.filter(target__in=t,report_date__gte=start_date,report_date__lte=end_date).aggregate(Sum('value'))
-            if not result['value__sum']:
-                i['requested_result'] = 0
-                i['result'] = 0
+            if start_date:
+                result = Report.objects.filter(target__in=t,report_date__gte=start_date,report_date__lte=end_date).aggregate(Sum('value'))
+                if not result['value__sum']:
+                    i['requested_result'] = 0
+                    i['result'] = 0
+                else:
+                    i['requested_result'] = result['value__sum']
+                    i['result'] = result['value__sum']
             else:
-                i['requested_result'] = result['value__sum']
-                i['result'] = result['value__sum']
+                i['requested_result'] = i['total_result']
+                i['result'] = i['total_result']
 
             i['last_updated_at'] = naturalday(t.last().last_updated_at)
             i['last_updated_by'] = last_updated_by
@@ -283,7 +314,7 @@ def target_stats(request):
             i['last_updated_at'] = None
 
 
-        results.append(i)
+        results['#target_id_'+str(i['id'])] = i
 
 
     return JsonResponse({'programs':total_number_of_programs,
