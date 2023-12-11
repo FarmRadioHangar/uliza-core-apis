fa17374276fa5847404d5603968eac3efb82bfa8
Jigsa <jiks.tes@gmail.com>
2023-12-11 17:37:35 +0300

    filter map request by country

--- django-api/log_app/views/vMapRequests.py
+++ django-api/log_app/views/vMapRequests.py
@@ -6,7 +6,15 @@ from rest_framework.views import APIView
 from log_app.models import MapRequest
 from log_app.serializers import MapRequestSerializer
 from rest_framework import filters
+import django_filters
 from django_filters.rest_framework import DjangoFilterBackend
+
+class MapRequestFilter(filters.FilterSet):
+    country = django_filters.NumberFilter(name='emitters__radio_station__country')
+
+    class Meta:
+        model = MapRequest
+        fields = ['id','emitters','status','country']
 
 class MapRequestGet(generics.ListCreateAPIView):
 
@@ -14,6 +22,7 @@ class MapRequestGet(generics.ListCreateAPIView):
     model = MapRequest
     serializer_class = MapRequestSerializer
     filter_backends = (filters.OrderingFilter, DjangoFilterBackend,)
+    filter_class = MapRequestFilter
     filter_fields = ['id','emitters','status']
     ordering_fields = ('id')
 
