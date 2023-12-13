4ded0b5f5450074aebb6da7adc76c1984c82caee
Jigsa <jiks.tes@gmail.com>
2023-12-13 14:36:28 +0300

    ordering for mapping request

--- django-api/log_app/views/vMapRequests.py
+++ django-api/log_app/views/vMapRequests.py
@@ -3,18 +3,11 @@ from rest_framework import generics
 from rest_framework import status
 from rest_framework.exceptions import NotFound
 from rest_framework.views import APIView
-from log_app.models import MapRequest
+from log_app.models import MapRequest,RadioStation
 from log_app.serializers import MapRequestSerializer
 from rest_framework import filters
 import django_filters
 from django_filters.rest_framework import DjangoFilterBackend
-
-class MapRequestFilter(filters.FilterSet):
-    country = django_filters.NumberFilter(name='emitters__radio_station__country')
-
-    class Meta:
-        model = MapRequest
-        fields = ['id','emitters','status','country']
 
 class MapRequestGet(generics.ListCreateAPIView):
 
@@ -22,9 +15,18 @@ class MapRequestGet(generics.ListCreateAPIView):
     model = MapRequest
     serializer_class = MapRequestSerializer
     filter_backends = (filters.OrderingFilter, DjangoFilterBackend,)
-    filter_class = MapRequestFilter
     filter_fields = ['id','emitters','status']
-    ordering_fields = ('id')
+    ordering_fields = ('id','status')
+
+    def get_queryset(self):
+        country = self.request.GET.get('country')
+        if country:
+            stations = RadioStation.objects.filter(country = country)
+            queryset = MapRequest.objects.filter(emitters__radio_station__in=stations).order_by('-id')
+        else:
+            queryset = MapRequest.objects.all().order_by('-id')
+
+        return queryset
 
 class MapRequestEntity(generics.RetrieveUpdateAPIView):
     queryset = MapRequest.objects.all()
